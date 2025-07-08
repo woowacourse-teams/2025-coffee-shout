eca6e32428e8a82cd7504aee349c84c3f0037026
HyeonsuLee <leehyeonsu4888@naver.com>
2025-07-08 14:59:03 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/coffeeshout/coffeeshout/CoffeeShoutApplication.java
@@ -0,0 +1,13 @@
+package coffeeshout.coffeeshout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeShoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeShoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coffeeshout/coffeeshout/CoffeeShoutApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeeshout.coffeeshout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
