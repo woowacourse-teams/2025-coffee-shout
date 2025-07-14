5fa27d82c9d2651a0ea3e9f05a10ef7c4cd05db0
7i.w__k <tizmfns1218@Naver.com>
2025-07-14 17:03:14 +0900

    feat: 초대코드 생성 로직 작성

---
+++ backend/src/test/java/coffeeshout/coffeeshout/domain/JoinCodeTest.java
@@ -0,0 +1,27 @@
+package coffeeshout.coffeeshout.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class JoinCodeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ABCDE", "A2B2C"})
+    void 조인코드는_규칙에_맞게_생성된다(String address) {
+        // given
+        JoinCode result = new JoinCode(address);
+
+        // when & then
+        assertThat(result).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ABCDEF", "A1LB2", "#ABCD"})
+    void 조인코드가_규칙에_맞지_않는다면_예외를_발생한다(String address) {
+        assertThatThrownBy(() -> new JoinCode(address))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
