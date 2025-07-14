25566dc3256df2d50fdc338620077b8bf60d4eb4
Ko Daeun <drechsu@gmail.com>
2025-07-14 19:57:03 +0900

    style : 글로벌 스타일 테마 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme";
+
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Hello, World!</h1>
+      </div>
+    </ThemeProvider>
   );
 };
 
 export default App;
+
+
+
+
---
+++ frontend/src/styles/emotion.d.ts
@@ -0,0 +1,10 @@
+import '@emotion/react'
+import { theme } from './theme'
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: typeof theme.colors;
+    typography: typeof theme.typography;
+  }
+}
+
