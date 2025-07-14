7305d470ebdf639fe565163a90692150059bea11
Ko Daeun <drechsu@gmail.com>
2025-07-14 17:48:43 +0900

    chore : 스토리북 설치

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import typescript from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
@@ -7,65 +10,61 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 
-export default [
-  js.configs.recommended,
-  {
-    files: ['webpack.common.js', 'webpack.prod.js', 'webpack.dev.js'],
-    languageOptions: {
+export default [js.configs.recommended, {
+  files: ['webpack.common.js', 'webpack.prod.js', 'webpack.dev.js'],
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    globals: {
+      ...globals.node,
+    },
+  },
+}, {
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    parser: typescriptParser,
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
       ecmaVersion: 2021,
       sourceType: 'module',
-      globals: {
-        ...globals.node,
-      },
+    },
+    globals: {
+      ...globals.browser,
     },
   },
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: 2021,
-        sourceType: 'module',
+  plugins: {
+    '@typescript-eslint': typescript,
+    react,
+    'react-hooks': reactHooks,
+    prettier,
+  },
+  rules: {
+    ...typescript.configs.recommended.rules,
+    ...react.configs.recommended.rules,
+    ...reactHooks.configs.recommended.rules,
+    ...prettierConfig.rules,
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'func-style': ['error', 'expression'],
+    'prefer-arrow-callback': 'error',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
       },
-      globals: {
-        ...globals.browser,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescript,
-      react,
-      'react-hooks': reactHooks,
-      prettier,
-    },
-    rules: {
-      ...typescript.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'func-style': ['error', 'expression'],
-      'prefer-arrow-callback': 'error',
-      'react/function-component-definition': [
-        'error',
-        {
-          namedComponents: 'arrow-function',
-          unnamedComponents: 'arrow-function',
-        },
-      ],
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-];
+}, ...storybook.configs["flat/recommended"]];
