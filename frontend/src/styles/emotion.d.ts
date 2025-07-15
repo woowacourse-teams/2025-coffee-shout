e1a5a93a9218ee9dd2b4ca3ed6431fd0bcd5eb6f
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-07-15 13:24:27 +0900

    [Feat] 공통 스타일(theme) 적용  (#31)
    * chore : css-loader, style-loader 설치
    
    * style : reset.css 설정
    
    * feat : 프리텐다드 폰트 적용
    
    * style : 글로벌 스타일 테마 설정
    
    * chore: 줄바꿈(LF) 통일
    
    * chore: 줄바꿈(LF) 통일
    
    * refactor : point 컬러 표현 통일
    
    * refactor : interface -> type 으로 변경
    
    * refactor : 스타일 통일
    
    * refactor : reset.css 출처 추가

---
+++ frontend/src/styles/emotion.d.ts
@@ -0,0 +1,10 @@
+import '@emotion/react'
+import { theme } from './theme'
+
+declare module '@emotion/react' {
+  export type Theme = {
+    color: typeof theme.color;
+    typography: typeof theme.typography;
+  }
+}
+
