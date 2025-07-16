858128b4f985416584a06ef6f4a53259833160c6
HyeonsuLee <leehyeonsu4888@naver.com>
2025-07-16 20:36:56 +0900

    refactor: RouletteRanges 리팩터링

--- backend/src/main/java/coffeeshout/coffeeshout/domain/roulette/RoulettePlayService.java
+++ backend/src/main/java/coffeeshout/coffeeshout/domain/roulette/RoulettePlayService.java
@@ -10,7 +10,5 @@ public class RoulettePlayService {
     public Player playRoulette(Player host, Room room) {
         state(room.isHost(host), "룰렛은 호스트만 시작할 수 있습니다.");
         return room.startRoulette();
-        // TODO: setPlaying 이름 바꾸기, 여기에 예외처리 넣기
-        // TODO: 룰렛 실행시키기
     }
 }
--- backend/src/main/java/coffeeshout/coffeeshout/domain/roulette/RouletteRanges.java
+++ backend/src/main/java/coffeeshout/coffeeshout/domain/roulette/RouletteRanges.java
@@ -7,25 +7,26 @@ import java.util.List;
 
 public class RouletteRanges {
 
-    private final List<RouletteRange> values = new ArrayList<>();
+    private final List<RouletteRange> ranges = new ArrayList<>();
 
-    public RouletteRanges(PlayersWithProbability playersWithProbability) {
-        playersWithProbability.forEach((player, probability) -> {
-            int start = endValue() + 1;
-            values.add(new RouletteRange(start, start + probability.value(), player));
-        });
+    public RouletteRanges(PlayersWithProbability probabilities) {
+        probabilities.forEach((player, probability) -> generateRange(endValue() + 1, probability.value(), player));
     }
 
     public Player pickPlayer(int number) {
-        return values.stream().filter(rouletteRange -> rouletteRange.isBetween(number)).findFirst()
+        return ranges.stream().filter(rouletteRange -> rouletteRange.isBetween(number)).findFirst()
                 .orElseThrow(() -> new IllegalStateException("범위에 해당하지 않는 숫자입니다."))
                 .player();
     }
 
     public int endValue() {
-        if (values.isEmpty()) {
+        if (ranges.isEmpty()) {
             return 0;
         }
-        return values.getLast().end();
+        return ranges.getLast().end();
+    }
+
+    private RouletteRange generateRange(int start, int gap, Player player) {
+        return new RouletteRange(start, start + gap, player);
     }
 }
