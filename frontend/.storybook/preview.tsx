13dee210bf05b8a16f0d4ad6a5a8523dd27c1a60
Ko Daeun <drechsu@gmail.com>
2025-07-16 18:07:50 +0900

    test : 스토리북 preview에 reset.css global.css 적용

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import type { Preview } from '@storybook/react-webpack5';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '../src/styles/theme';
+import '../src/styles/reset.css';
+import '../src/styles/global.css';
 
 const preview: Preview = {
   decorators: [
