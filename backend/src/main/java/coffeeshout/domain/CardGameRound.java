8c8ec0daa020ebcc44cf366858570f93325a48f4
MinJun Choi <mj043000@naver.com>
2025-07-16 13:51:56 +0900

    refactor: CardGameRound의 value 필드를 final로 변경
    Co-authored-by: junhaa <wooha34567@naver.com>

--- backend/src/main/java/coffeeshout/domain/CardGameRound.java
+++ backend/src/main/java/coffeeshout/domain/CardGameRound.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum CardGameRound {
     ONE(1), TWO(2), END(-1);
 
-    private Integer value;
+    final private Integer value;
 
     CardGameRound(Integer value) {
         this.value = value;
