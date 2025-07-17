e64a32607d02e692912bf8a9233b3fd319938d8f
Ko Daeun <drechsu@gmail.com>
2025-07-17 21:16:38 +0900

    fix : App 컴포넌트에서 ToggleButton 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
 import { ThemeProvider } from '@emotion/react';
-import ToggleButton from './components/@common/ToggleButton/ToggleButton';
 import { theme } from './styles/theme';
 
 const App = () => {
-  const [selectedOption, setSelectedOption] = useState('참가자');
-  const handleClickOption = (option: string) => {
-    setSelectedOption(option);
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <h1>Hello, World!</h1>
-        <ToggleButton
-          options={['참가자', '룰렛', '미니게임']}
-          selectedOption={selectedOption}
-          onSelectOption={handleClickOption}
-        />
       </div>
     </ThemeProvider>
   );
