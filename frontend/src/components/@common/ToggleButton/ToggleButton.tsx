5c55990c7004ac3fc3664eb7569cb771930fdf63
Ko Daeun <drechsu@gmail.com>
2025-07-17 21:30:11 +0900

    refactor : 제네릭으로 선택된 옵션이 주어진 옵션 배열에서만 가능하도록 설정

--- frontend/src/components/@common/ToggleButton/ToggleButton.tsx
+++ frontend/src/components/@common/ToggleButton/ToggleButton.tsx
@@ -1,13 +1,17 @@
 import Headline4 from '../Headline4/Headline4';
 import * as S from './ToggleButton.styled';
 
-type Props = {
-  options: string[];
-  selectedOption: string;
-  onSelectOption: (option: string) => void;
+type Props<Option extends string> = {
+  options: Option[];
+  selectedOption: Option;
+  onSelectOption: (option: Option) => void;
 };
 
-const ToggleButton = ({ options, selectedOption, onSelectOption }: Props) => {
+const ToggleButton = <Option extends string>({
+  options,
+  selectedOption,
+  onSelectOption,
+}: Props<Option>) => {
   const selectedIndex = options.indexOf(selectedOption);
 
   return (
