743f2dfa36fb8c3c7d27226aff2d4095b453eb27
sooyeoniya <tndus1306403@naver.com>
2025-07-18 22:37:58 +0900

    feat: CardBack 버튼으로 변경 및 클릭 시 눌리는 효과 추가

--- frontend/src/features/miniGame/cardGame/components/CardBack/CardBack.styled.ts
+++ frontend/src/features/miniGame/cardGame/components/CardBack/CardBack.styled.ts
@@ -25,13 +25,34 @@ const circleVariants = {
   large: '17.3vw',
 };
 
-export const Container = styled.div<Props>`
+export const Container = styled.button<Props>`
   ${({ $size }) => cardVariants[$size || 'large']}
   border: 2px solid ${({ theme }) => theme.color.point[200]};
   background-color: ${({ theme }) => theme.color.point[400]};
   border-radius: 7px;
   box-shadow: 0 3px 3px rgba(0, 0, 0, 0.4);
   position: relative;
+  cursor: pointer;
+  transition:
+    transform 0.2s ease,
+    box-shadow 0.2s ease;
+
+  &:active {
+    transform: scale(0.98);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  }
+
+  /* 모바일 터치 효과 제거 */
+  -webkit-tap-highlight-color: transparent;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  outline: none;
+
+  /* 더블클릭 및 더블탭 확대 차단 */
+  touch-action: manipulation;
 `;
 
 export const Circle = styled.div<Props>`
--- frontend/src/features/miniGame/cardGame/components/CardBack/CardBack.tsx
+++ frontend/src/features/miniGame/cardGame/components/CardBack/CardBack.tsx
@@ -1,12 +1,14 @@
+import { ComponentProps } from 'react';
 import * as S from './CardBack.styled';
 
 type Props = {
   size?: 'small' | 'medium' | 'large';
-};
+  onClick: () => void;
+} & Omit<ComponentProps<'button'>, 'onClick'>;
 
-const CardBack = ({ size }: Props) => {
+const CardBack = ({ size, onClick, ...rest }: Props) => {
   return (
-    <S.Container $size={size}>
+    <S.Container $size={size} onClick={onClick} {...rest}>
       <S.Circle $size={size}>
         <S.CoffeeIcon src={'/images/coffee-white.svg'} alt="coffee-icon" />
       </S.Circle>
