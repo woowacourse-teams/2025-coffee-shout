06ec64fec5653b628cbbf544b311fee3eeee0dde
Ko Daeun <drechsu@gmail.com>
2025-07-18 11:51:42 +0900

    test : 스토리북 variant 중심으로 테스트하도록 변경

--- frontend/src/components/@common/Button/Button.stories.tsx
+++ frontend/src/components/@common/Button/Button.stories.tsx
@@ -5,6 +5,16 @@ const meta: Meta<typeof Button> = {
   title: 'Common/Button',
   component: Button,
   tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'disabled', 'loading'],
+    },
+    height: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
 };
 export default meta;
 
@@ -13,44 +23,44 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: {
     variant: 'primary',
-    children: '게임 시작',
-    width: '328px',
-    height: '50px',
-  },
-};
-
-export const Loading: Story = {
-  args: {
-    variant: 'loading',
-    children: '게임 대기중',
-    width: '328px',
-    height: '50px',
+    children: 'Primary',
+    width: '120px',
+    height: 'large',
   },
 };
 
 export const Secondary: Story = {
   args: {
     variant: 'secondary',
-    children: '취소',
-    width: '138px',
-    height: '45px',
+    children: 'Secondary',
+    width: '120px',
+    height: 'large',
   },
 };
 
-export const PrimaryChange: Story = {
+export const Disabled: Story = {
   args: {
-    variant: 'primary',
-    children: '변경',
-    width: '138px',
-    height: '45px',
+    variant: 'disabled',
+    children: 'Disabled',
+    width: '120px',
+    height: 'large',
   },
 };
 
-export const PrimaryWithIcon: Story = {
+export const Loading: Story = {
+  args: {
+    variant: 'loading',
+    children: 'Loading',
+    width: '120px',
+    height: 'large',
+  },
+};
+
+export const IconButton: Story = {
   args: {
     variant: 'primary',
     children: <img src={'/images/share-icon.svg'} alt="icon" />,
-    width: '50px',
-    height: '50px',
+    width: '40px',
+    height: 'small',
   },
 };
