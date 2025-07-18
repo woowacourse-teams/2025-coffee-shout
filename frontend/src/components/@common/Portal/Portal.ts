ba12779652fa3ed0a2031789d4e743523645db0d
sooyeoniya <tndus1306403@naver.com>
2025-07-18 11:35:31 +0900

    refactor: Portal에 containerId 기본값 지정 제거
    기본값 없는 경우 document.body 하위에 붙도록 수정

--- frontend/src/components/@common/Portal/Portal.ts
+++ frontend/src/components/@common/Portal/Portal.ts
@@ -5,11 +5,11 @@ type Props = {
   containerId?: string;
 } & PropsWithChildren;
 
-const Portal = ({ children, containerId = 'root' }: Props) => {
+const Portal = ({ children, containerId }: Props) => {
   const [container, setContainer] = useState<HTMLElement | null>(null);
 
   useEffect(() => {
-    const targetContainer = document.getElementById(containerId) || document.body;
+    const targetContainer = containerId ? document.getElementById(containerId) : document.body;
     setContainer(targetContainer);
   }, [containerId]);
 
