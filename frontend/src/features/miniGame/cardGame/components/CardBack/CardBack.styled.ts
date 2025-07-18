8e5d43bf79bcd52966898ece3aea8bff43b1c79d
sooyeoniya <tndus1306403@naver.com>
2025-07-18 22:47:57 +0900

    refactor: 카드 variants 속성 상수 파일로 분리

--- frontend/src/features/miniGame/cardGame/components/CardBack/CardBack.styled.ts
+++ frontend/src/features/miniGame/cardGame/components/CardBack/CardBack.styled.ts
@@ -1,38 +1,20 @@
 import styled from '@emotion/styled';
+import { cardVariants, circleVariants } from '../../constants/variants';
 
 type Props = {
   $size?: 'small' | 'medium' | 'large';
-};
-
-const cardVariants = {
-  small: {
-    width: '12vw',
-    height: '15.36vw',
-  },
-  medium: {
-    width: '16vw',
-    height: '20.48vw',
-  },
-  large: {
-    width: '24vw',
-    height: '30.72vw',
-  },
-};
-
-const circleVariants = {
-  small: '8.6vw',
-  medium: '11.5vw',
-  large: '17.3vw',
 };
 
 export const Container = styled.button<Props>`
   ${({ $size }) => cardVariants[$size || 'large']}
   border: 2px solid ${({ theme }) => theme.color.point[200]};
   background-color: ${({ theme }) => theme.color.point[400]};
+
   border-radius: 7px;
   box-shadow: 0 3px 3px rgba(0, 0, 0, 0.4);
   position: relative;
   cursor: pointer;
+
   transition:
     transform 0.2s ease,
     box-shadow 0.2s ease;
@@ -41,24 +23,13 @@ export const Container = styled.button<Props>`
     transform: scale(0.98);
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
   }
-
-  /* 모바일 터치 효과 제거 */
-  -webkit-tap-highlight-color: transparent;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  outline: none;
-
-  /* 더블클릭 및 더블탭 확대 차단 */
-  touch-action: manipulation;
 `;
 
 export const Circle = styled.div<Props>`
   background-color: ${({ theme }) => theme.color.point[300]};
   width: ${({ $size }) => circleVariants[$size || 'large']};
   height: ${({ $size }) => circleVariants[$size || 'large']};
+
   border-radius: 50%;
   margin: auto;
   position: absolute;
---
+++ frontend/src/features/miniGame/cardGame/constants/variants.ts
@@ -0,0 +1,20 @@
+export const cardVariants = {
+  small: {
+    width: '12vw',
+    height: '15.36vw',
+  },
+  medium: {
+    width: '16vw',
+    height: '20.48vw',
+  },
+  large: {
+    width: '24vw',
+    height: '30.72vw',
+  },
+};
+
+export const circleVariants = {
+  small: '8.6vw',
+  medium: '11.5vw',
+  large: '17.3vw',
+};
