ec7ff196c4dec610f25365b1a81c106bb8bedae6
SuHwa Lee <tnghk9611@naver.com>
2025-07-18 13:26:19 +0900

    [feat] Input 컴포넌트 구현 (#69)
    * feat: input 공통 컴포넌트 구현
    
    * feat: input 컴포넌트 스토리북 작성
    
    * chore: x 아이콘 추가
    
    * refactor: 변수명 수정
    
    * style: 텍스트 입력 시 하단 border 색상 수정
    
    * refactor: props 이름 수정
    
    * refactor: 화살표 함수가 아닌 함수명을 바로 넘겨주는 방식으로 수정
    
    * refactor: Componentprops를 뒤에 작성하는 구조로 수정
    
    * refactor: disabled일 때 cursor 속성을 default로 수정
    
    * style: 닫기 이미지에 드래그 막는 속성 추가 및 중앙 정렬

---
+++ frontend/src/components/@common/Input/Input.tsx
@@ -0,0 +1,27 @@
+import { ComponentProps } from 'react';
+import * as S from './Input.styled';
+
+type Props = {
+  height?: string;
+  onClear: () => void;
+} & ComponentProps<'input'>;
+
+const Input = ({ height = '32px', onClear, value, onChange, ref, ...rest }: Props) => {
+  const hasValue = Boolean(value && String(value).length > 0);
+
+  return (
+    <S.Container height={height} hasValue={hasValue}>
+      <S.Input ref={ref} value={value} onChange={onChange} {...rest} />
+      <S.ClearButton
+        type="button"
+        onClick={onClear}
+        aria-label="입력 내용 지우기"
+        hasValue={hasValue}
+      >
+        <S.CloseIcon src={'/images/close-icon.svg'} />
+      </S.ClearButton>
+    </S.Container>
+  );
+};
+
+export default Input;
