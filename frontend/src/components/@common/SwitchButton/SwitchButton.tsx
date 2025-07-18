54ecf8072c066f50510c8579b44aa70d261b1690
Ko Daeun <drechsu@gmail.com>
2025-07-18 09:55:01 +0900

    refactor : 이미지 경로 객체 상수로 분리

--- frontend/src/components/@common/SwitchButton/SwitchButton.tsx
+++ frontend/src/components/@common/SwitchButton/SwitchButton.tsx
@@ -1,14 +1,18 @@
 import { ComponentProps } from 'react';
 import * as S from './SwitchButton.styled';
 
+const ICONS = {
+  statistics: '/images/statistics-icon.svg' as const,
+  roulette: '/images/roulette-icon.svg' as const,
+};
+
 type Props = {
   currentView: 'statistics' | 'roulette';
   onClick: () => void;
 } & Omit<ComponentProps<'button'>, 'onClick'>;
 
 const SwitchButton = ({ currentView, onClick, ...rest }: Props) => {
-  const currentIcon =
-    currentView === 'statistics' ? '/images/statistics-icon.svg' : '/images/roulette-icon.svg';
+  const currentIcon = ICONS[currentView];
 
   return (
     <S.Container onClick={onClick} {...rest}>
