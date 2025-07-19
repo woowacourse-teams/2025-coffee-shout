22e396778e2a85f274849b7b0c4362f53e1cf183
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-07-19 18:35:26 +0900

    [feat] PlayerCard 컴포넌트 구현 (#103)
    * chore: profile 이미지 추가
    
    * feat: PlayerCard 컴포넌트 구현
    
    * chore: styled 파일 tsx->ts 변경
    
    * feat: 사용자 이름이 긴 경우 대응
    
    사용자 이름이 긴 경우에 '...'으로 표시되도록 스타일 수정
    긴 사용자에 대한 스토리 2개 추가
    
    * refactor: PlayerIcon 이미지 경로 대신 색상 받도록 수정

---
+++ frontend/src/components/@composition/PlayerCard/PlayerCard.styled.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 15px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  flex: 1;
+  min-width: 0;
+`;
+
+export const NameWrapper = styled.div`
+  padding-right: 20px;
+  flex: 1;
+  min-width: 0;
+
+  h4 {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const PlayerIcon = styled.img`
+  width: 50px;
+  height: 50px;
+`;
---
+++ frontend/src/components/@composition/PlayerCard/PlayerCard.tsx
@@ -0,0 +1,27 @@
+import Headline4 from '@/components/@common/Headline4/Headline4';
+import { PropsWithChildren } from 'react';
+import * as S from './PlayerCard.styled';
+
+// TODO: 색상 추가 필요
+type IconColor = 'red';
+
+type Props = {
+  name: string;
+  iconColor: IconColor;
+} & PropsWithChildren;
+
+const PlayerCard = ({ name, iconColor, children }: Props) => {
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.PlayerIcon src={`/images/profile-${iconColor}.svg`} alt={`player-${name}-icon`} />
+        <S.NameWrapper>
+          <Headline4>{name}</Headline4>
+        </S.NameWrapper>
+      </S.Wrapper>
+      {children}
+    </S.Container>
+  );
+};
+
+export default PlayerCard;
