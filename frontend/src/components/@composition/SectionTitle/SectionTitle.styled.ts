796912bd30e69fd3364278d57ee38a7aab70c1ba
Ko Daeun <drechsu@gmail.com>
2025-07-20 21:57:33 +0900

    styled : SectionTitle 상단  패딩 값 감소

--- frontend/src/components/@composition/SectionTitle/SectionTitle.styled.ts
+++ frontend/src/components/@composition/SectionTitle/SectionTitle.styled.ts
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  padding: 16px;
+  padding: 8px 16px 16px 16px;
   display: flex;
   flex-direction: column;
   gap: 12px;
