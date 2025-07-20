ab8dd000c3dc6efd90eb7ee3f2f66d0c4046772a
Ko Daeun <drechsu@gmail.com>
2025-07-20 21:22:46 +0900

    feat : RouletteSecton 구현

---
+++ frontend/src/pages/LobbyPage/RouletteSection/RouletteSection.styled.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const SwitchButtonWrapper = styled.div`
+  position: absolute;
+  top: 0.3rem;
+  right: 0.3rem;
+`;
---
+++ frontend/src/pages/LobbyPage/RouletteSection/RouletteSection.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import SwitchButton from '@/components/@common/SwitchButton/SwitchButton';
+import SectionTitle from '@/components/@composition/SectionTitle/SectionTitle';
+import { RouletteWheel } from './RouletteWheel/RouletteWheel';
+import { ProbabilityList } from './ProbabilityList/ProbabilityList';
+import * as S from './RouletteSection.styled';
+
+type RouletteView = 'roulette' | 'statistics';
+
+export const RouletteSection = () => {
+  const [currentView, setCurrentView] = useState<RouletteView>('roulette');
+
+  const handleViewChange = () => {
+    setCurrentView(currentView === 'roulette' ? 'statistics' : 'roulette');
+  };
+
+  const renderContent = () => {
+    switch (currentView) {
+      case 'roulette':
+        return <RouletteWheel />;
+      case 'statistics':
+        return <ProbabilityList />;
+      default:
+        return <RouletteWheel />;
+    }
+  };
+
+  return (
+    <>
+      <SectionTitle title="룰렛" description="미니게임을 통해 당첨 확률이 조정됩니다" />
+      <S.SwitchButtonWrapper>
+        <SwitchButton currentView={currentView} onClick={handleViewChange} />
+      </S.SwitchButtonWrapper>
+      {renderContent()}
+    </>
+  );
+};
