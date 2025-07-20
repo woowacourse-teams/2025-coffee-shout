382b5a36d210b50b963bb1ca9477ac43d9946a66
SuHwa Lee <tnghk9611@naver.com>
2025-07-20 18:43:46 +0900

    feat: 게임 대기 화면 구현

---
+++ frontend/src/pages/MiniGameReadyPage/MiniGameReadyPage.styled.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 20px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  margin-bottom: 30px;
+`;
+
+export const Time = styled.span`
+  font-size: 150px;
+  font-weight: 900;
+  color: white;
+`;
--- frontend/src/pages/MiniGameReadyPage/MiniGameReadyPage.tsx
+++ frontend/src/pages/MiniGameReadyPage/MiniGameReadyPage.tsx
@@ -1,18 +1,23 @@
+import Headline1 from '@/components/@common/Headline1/Headline1';
+import Layout from '@/layouts/Layout';
+import * as S from './MiniGameReadyPage.styled';
+import Description from '@/components/@common/Description/Description';
+
 const MiniGameReadyPage = () => {
   return (
-    <div>
-      <h1>미니게임 준비</h1>
-      <p>미니게임을 시작하기 전 준비 화면입니다.</p>
-      <div>
-        <h3>게임 규칙</h3>
-        <ul>
-          <li>규칙 1</li>
-          <li>규칙 2</li>
-          <li>규칙 3</li>
-        </ul>
-      </div>
-      <button>준비 완료</button>
-    </div>
+    <Layout color="point-400">
+      <S.Container>
+        <S.TextContainer>
+          <Headline1 color="white">곧 게임이 시작돼요</Headline1>
+          <Description color="white">
+            게임이 시작될 때까지
+            <br />
+            조금만 기다려주세요
+          </Description>
+        </S.TextContainer>
+        <S.Time>3</S.Time>
+      </S.Container>
+    </Layout>
   );
 };
 
