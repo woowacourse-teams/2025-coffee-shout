242079e9d9b7c4ef0db4a1a8bb89e134bc2fdc41
SuHwa Lee <tnghk9611@naver.com>
2025-07-20 18:49:32 +0900

    feat: 3초 뒤에 게임 시작화면으로 이동하도록 구현

--- frontend/src/pages/MiniGameReadyPage/MiniGameReadyPage.tsx
+++ frontend/src/pages/MiniGameReadyPage/MiniGameReadyPage.tsx
@@ -1,9 +1,30 @@
+import Description from '@/components/@common/Description/Description';
 import Headline1 from '@/components/@common/Headline1/Headline1';
 import Layout from '@/layouts/Layout';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import * as S from './MiniGameReadyPage.styled';
-import Description from '@/components/@common/Description/Description';
 
 const MiniGameReadyPage = () => {
+  const [countdown, setCountdown] = useState(3);
+  const navigate = useNavigate();
+  const { roomId, miniGameId } = useParams();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prev) => {
+        if (prev <= 1) {
+          clearInterval(timer);
+          navigate(`/room/${roomId}/${miniGameId}/play`);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [navigate, roomId, miniGameId]);
+
   return (
     <Layout color="point-400">
       <S.Container>
@@ -15,7 +36,7 @@ const MiniGameReadyPage = () => {
             조금만 기다려주세요
           </Description>
         </S.TextContainer>
-        <S.Time>3</S.Time>
+        <S.Time>{countdown}</S.Time>
       </S.Container>
     </Layout>
   );
