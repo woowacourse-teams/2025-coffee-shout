8ef5e3f22301d58598f07166330529871058a1ec
Ko Daeun <drechsu@gmail.com>
2025-07-20 21:23:22 +0900

    feat : LobbyPage 구현

--- frontend/src/pages/LobbyPage/LobbyPage.tsx
+++ frontend/src/pages/LobbyPage/LobbyPage.tsx
@@ -1,18 +1,63 @@
+import React, { useState } from 'react';
+import BackButton from '@/components/@common/BackButton/BackButton';
+import Button from '@/components/@common/Button/Button';
+import ToggleButton from '@/components/@common/ToggleButton/ToggleButton';
+import Layout from '@/layouts/Layout';
+import { ParticipantSection } from './ParticipantSection/ParticipantSection';
+import { RouletteSection } from './RouletteSection/RouletteSection';
+import { MiniGameSection } from './MiniGameSection/MiniGameSection';
+import * as S from './LobbyPage.styled';
+
+type SectionType = '참가자' | '룰렛' | '미니게임';
+
 const LobbyPage = () => {
+  const [currentSection, setCurrentSection] = useState<SectionType>('참가자');
+
+  const handleClickButton = () => {
+    console.log('게임 시작!');
+  };
+
+  const handleSectionChange = (option: string) => {
+    setCurrentSection(option as SectionType);
+  };
+
+  const renderSection = () => {
+    switch (currentSection) {
+      case '참가자':
+        return <ParticipantSection />;
+      case '룰렛':
+        return <RouletteSection />;
+      case '미니게임':
+        return <MiniGameSection />;
+      default:
+        return <ParticipantSection />;
+    }
+  };
+
   return (
-    <div>
-      <h1>게임 로비</h1>
-      <p>게임 참여자들이 모이는 공간입니다.</p>
-      <div>
-        <h3>참여자 목록</h3>
-        <ul>
-          <li>참여자 1</li>
-          <li>참여자 2</li>
-          <li>참여자 3</li>
-        </ul>
-      </div>
-      <button>게임 시작</button>
-    </div>
+    <Layout>
+      <Layout.TopBar left={<BackButton onClick={() => {}} />} />
+      <Layout.Content>
+        <S.Container>
+          {renderSection()}
+          <S.Wrapper>
+            <ToggleButton
+              options={['참가자', '룰렛', '미니게임']}
+              selectedOption={currentSection}
+              onSelectOption={handleSectionChange}
+            />
+          </S.Wrapper>
+        </S.Container>
+      </Layout.Content>
+      <Layout.ButtonBar flexRatios={[5.5, 1]}>
+        <Button variant="primary" onClick={handleClickButton}>
+          게임 시작
+        </Button>
+        <Button variant="primary" onClick={handleClickButton}>
+          <img src="/images/share-icon.svg" alt="공유" />
+        </Button>
+      </Layout.ButtonBar>
+    </Layout>
   );
 };
 
