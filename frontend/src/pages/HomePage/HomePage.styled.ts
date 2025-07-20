513d8dfb4f89c4c4f8fe1b75d9c90f40b2123ad9
SuHwa Lee <tnghk9611@naver.com>
2025-07-20 17:26:52 +0900

    feat: 홈 페이지 UI 구현

---
+++ frontend/src/pages/HomePage/HomePage.styled.ts
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const BannerContainer = styled.div`
+  height: 100%;
+  padding: 40px;
+  line-height: 30px;
+  position: relative;
+`;
+
+export const Logo = styled.img`
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,34 @@
+import Headline3 from '@/components/@common/Headline3/Headline3';
+import Layout from '@/layouts/Layout';
+import * as S from './HomePage.styled';
+import RoomActionButton from '@/components/@common/RoomActionButton/RoomActionButton';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Coffee Shout</h1>
-      <p>Splash(3초) + 메인 화면</p>
-      <div>
-        <button>방 만들기</button>
-        <button>방 찾기</button>
-      </div>
-    </div>
+    <Layout>
+      <Layout.Banner>
+        <S.BannerContainer>
+          <Headline3 color="white">
+            초대받은 방에 참가하거나
+            <br />
+            새로운 방을 만들어보세요
+          </Headline3>
+          <S.Logo src="/images/logo-main.svg" />
+        </S.BannerContainer>
+      </Layout.Banner>
+      <S.ButtonContainer>
+        <RoomActionButton
+          title="방 만들기"
+          descriptions={['새로운 방을 만들어', '재미있는 커피내기를 시작해보세요 ']}
+          onClick={() => {}}
+        />
+        <RoomActionButton
+          title="방 참가하러 가기"
+          descriptions={['받은 초대 코드를 입력해서', '방으로 들어가보세요']}
+          onClick={() => {}}
+        />
+      </S.ButtonContainer>
+    </Layout>
   );
 };
 
