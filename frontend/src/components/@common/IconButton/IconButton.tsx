7aea79156b9dccdde0970bbe8fa588770392ba69
Ko Daeun <drechsu@gmail.com>
2025-07-21 13:49:01 +0900

    refactor : SwitchButton -> IconButton 으로 네이밍 변경
    iconSrc를 그대로 렌더링 하는 버튼으로 변경

---
+++ frontend/src/components/@common/IconButton/IconButton.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps } from 'react';
+import * as S from './IconButton.styled';
+
+type Props = {
+  iconSrc: string;
+  onClick: () => void;
+} & Omit<ComponentProps<'button'>, 'onClick'>;
+
+const IconButton = ({ iconSrc, onClick, ...rest }: Props) => {
+  return (
+    <S.Container onClick={onClick} {...rest}>
+      <S.Icon src={iconSrc} alt={iconSrc} />
+    </S.Container>
+  );
+};
+
+export default IconButton;
