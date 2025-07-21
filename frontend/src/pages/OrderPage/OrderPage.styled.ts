d6d7cb366054ede132225d73604b1fc28c9799a0
SuHwa Lee <tnghk9611@naver.com>
2025-07-21 21:07:29 +0900

    [feat] OrderPage 구현 (#117)
    * feat: SwitchButton props에 detail 타입 추가
    
    * chore: 상세보기 버튼 아이콘 추가
    
    * feat: 주문 페이지 구현
    
    * chore: 데이터 수정
    
    * refactor: SwitchButton을 IconButton으로 변경
    
    * feat: 메인 화면으로 이동하는 기능 추가

---
+++ frontend/src/pages/OrderPage/OrderPage.styled.ts
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+
+export const BannerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  color: white;
+`;
+
+export const IconWrapper = styled.div`
+  margin-bottom: 1.5rem;
+
+  svg {
+    width: 4rem;
+    height: 4rem;
+    stroke-width: 1.5;
+  }
+`;
+
+export const ListHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1.5rem;
+`;
+
+export const OrderList = styled.div`
+  margin-bottom: 1.5rem;
+`;
+
+export const OrderItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.75rem 0;
+`;
+
+export const Divider = styled.div`
+  border-top: 1px dashed #d1d5db;
+  margin: 1.5rem 0;
+`;
+
+export const TotalWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const DetailGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const DetailItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 0;
+`;
