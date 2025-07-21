42909830421f8779969cd438445d408dd6b9e7f8
Ko Daeun <drechsu@gmail.com>
2025-07-21 15:58:11 +0900

    styled : Divider의 margin-bottom과 width 변경

--- frontend/src/components/@common/Divider/Divider.tsx
+++ frontend/src/components/@common/Divider/Divider.tsx
@@ -7,7 +7,7 @@ type Props = {
   width?: string;
 };
 
-const Divider = ({ color = 'point-200', height = '1px', width = '100%' }: Props) => {
+const Divider = ({ color = 'point-200', height = '3px', width = '100%' }: Props) => {
   return <S.Container $color={COLOR_MAP[color]} $height={height} $width={width} />;
 };
 
