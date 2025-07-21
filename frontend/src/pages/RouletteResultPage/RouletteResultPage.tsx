4b5393b99ab48e9a90e910415b2ede3ab7273a21
SuHwa Lee <tnghk9611@naver.com>
2025-07-21 20:03:18 +0900

    refactor: winnerName 변수명을 winner로 수정

--- frontend/src/pages/RouletteResultPage/RouletteResultPage.tsx
+++ frontend/src/pages/RouletteResultPage/RouletteResultPage.tsx
@@ -6,13 +6,13 @@ import { useLocation } from 'react-router-dom';
 
 const RouletteResultPage = () => {
   const location = useLocation();
-  const winnerName = location.state?.winnerName || '당첨자';
+  const winner = location.state?.winner || '당첨자';
 
   return (
     <Layout color="point-400">
       <S.Container>
         <S.Logo src="/images/bread-logo-white.svg" />
-        <Headline1 color="white">{winnerName}</Headline1>
+        <Headline1 color="white">{winner}</Headline1>
         <Headline3 color="white">님이 당첨되었습니다!</Headline3>
       </S.Container>
     </Layout>
