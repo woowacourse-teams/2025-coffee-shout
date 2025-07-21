2d02b48a126d0c61f8b8b27185e7769e7e6b9e95
sooyeoniya <tndus1306403@naver.com>
2025-07-22 02:28:05 +0900

    docs: MiniGamePlayPage에 TODO 추가

--- frontend/src/pages/MiniGamePlayPage/MiniGamePlayPage.tsx
+++ frontend/src/pages/MiniGamePlayPage/MiniGamePlayPage.tsx
@@ -10,6 +10,10 @@ import * as S from './MiniGamePlayPage.styled';
 
 const TOTAL_COUNT = 10;
 
+// TODO: 게임 종류에 따라서 분기처리 되도록 수정 (이전 페이지에서 입력된 미니게임 종류를 토대로 화면이 바뀌어야 함 - 미니게임 종류에 대하여 Context로 관리 필요)
+// TODO: 라운드가 총 2개이므로 2개의 라운드에 맞춰 이동 루트 추가
+// TODO: 카드를 하나 선택했을 때 다음 페이지로 이동할 수 있도록 수정 (당장은 싱글 플레이이므로 이 로직 자체도 추후 수정되어야 함)
+
 const MiniGamePlayPage = () => {
   const [current, setCurrent] = useState(TOTAL_COUNT);
   const navigate = useNavigate();
