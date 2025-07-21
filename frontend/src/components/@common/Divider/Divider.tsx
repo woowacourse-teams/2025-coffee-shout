7d1dbc78d29583d3c11190bc227f8b56ff68af41
Ko Daeun <drechsu@gmail.com>
2025-07-21 15:21:56 +0900

    refactor : Props 네이밍 통일

--- frontend/src/components/@common/Divider/Divider.tsx
+++ frontend/src/components/@common/Divider/Divider.tsx
@@ -1,12 +1,12 @@
 import * as S from './Divider.styled';
 
-type DividerProps = {
+type Props = {
   color?: string;
   height?: string;
   width?: string;
 };
 
-const Divider = ({ color, height = '1px', width = '100%' }: DividerProps) => {
+const Divider = ({ color, height = '1px', width = '100%' }: Props) => {
   return <S.Container $color={color} $height={height} $width={width} />;
 };
 
