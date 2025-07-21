b73ffef18050d6277c37cb5166dd41849e037ac8
Ko Daeun <drechsu@gmail.com>
2025-07-21 15:50:13 +0900

    refactor : Divider 리팩토링
    - 스토리북에 보이게 변경
    - color를 다른 컴포넌트와 동일하게 받을 수 있도록 수정

--- frontend/src/components/@common/Divider/Divider.tsx
+++ frontend/src/components/@common/Divider/Divider.tsx
@@ -1,13 +1,14 @@
+import { COLOR_MAP, ColorKey } from '@/constants/color';
 import * as S from './Divider.styled';
 
 type Props = {
-  color?: string;
+  color?: ColorKey;
   height?: string;
   width?: string;
 };
 
-const Divider = ({ color, height = '1px', width = '100%' }: Props) => {
-  return <S.Container $color={color} $height={height} $width={width} />;
+const Divider = ({ color = 'point-200', height = '1px', width = '100%' }: Props) => {
+  return <S.Container $color={COLOR_MAP[color]} $height={height} $width={width} />;
 };
 
 export default Divider;
