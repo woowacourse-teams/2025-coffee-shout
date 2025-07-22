ac1945fc0552e6291d883894cf07f98430fdcf77
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-07-22 14:09:22 +0900

    Desing : 룰렛 가운데 오도록 수정 (#149)

--- frontend/src/pages/LobbyPage/RouletteSection/RouletteSection.styled.ts
+++ frontend/src/pages/LobbyPage/RouletteSection/RouletteSection.styled.ts
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const IconButtonWrapper = styled.div`
   position: absolute;
   top: 0.3rem;
