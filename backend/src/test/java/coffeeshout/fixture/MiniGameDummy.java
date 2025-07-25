7ef5648161b730034bcf9450026077cce7752448
MinJun Choi <mj043000@naver.com>
2025-07-25 21:00:40 +0900

    test: 테스트용 MiniGameDummy에 assignPlayers 메서드 구현
    Co-authored-by: kiwoook <tizmfns3@gmail.com>

--- backend/src/test/java/coffeeshout/fixture/MiniGameDummy.java
+++ backend/src/test/java/coffeeshout/fixture/MiniGameDummy.java
@@ -3,6 +3,7 @@ package coffeeshout.fixture;
 import coffeeshout.minigame.domain.MiniGameResult;
 import coffeeshout.room.domain.MiniGameType;
 import coffeeshout.room.domain.Playable;
+import coffeeshout.room.domain.player.Players;
 
 public class MiniGameDummy implements Playable {
 
@@ -19,5 +20,10 @@ public class MiniGameDummy implements Playable {
     @Override
     public MiniGameType getMiniGameType() {
         return null;
+    }
+
+    @Override
+    public void assignPlayers(Players players) {
+
     }
 }
