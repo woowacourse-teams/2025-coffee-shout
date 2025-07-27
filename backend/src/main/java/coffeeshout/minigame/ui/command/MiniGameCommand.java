3d66c895b81f2bde2ae284f6d4a994092c0cd122
junhaa <wooha34567@gmail.com>
2025-07-27 14:51:59 +0900

    refactor: MiniGameCommandDispatcher 관련 로직 테스트 추가
    - `MiniGameCommand` 인터페이스에서 불필요한 `getMiniGameType` 메서드 제거

--- backend/src/main/java/coffeeshout/minigame/ui/command/MiniGameCommand.java
+++ backend/src/main/java/coffeeshout/minigame/ui/command/MiniGameCommand.java
@@ -1,9 +1,5 @@
 package coffeeshout.minigame.ui.command;
 
 
-import coffeeshout.minigame.domain.MiniGameType;
-
 public interface MiniGameCommand {
-
-    MiniGameType getMiniGameType();
 }
--- backend/src/main/java/coffeeshout/minigame/ui/request/command/SelectCardCommand.java
+++ backend/src/main/java/coffeeshout/minigame/ui/request/command/SelectCardCommand.java
@@ -1,12 +1,6 @@
 package coffeeshout.minigame.ui.request.command;
 
-import coffeeshout.minigame.domain.MiniGameType;
 import coffeeshout.minigame.ui.command.MiniGameCommand;
 
 public record SelectCardCommand(String playerName, Integer cardIndex) implements MiniGameCommand {
-
-    @Override
-    public MiniGameType getMiniGameType() {
-        return MiniGameType.CARD_GAME;
-    }
 }
