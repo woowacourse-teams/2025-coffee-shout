7629e3748e5a2206eae28e20928174c304628519
SuHwa Lee <tnghk9611@naver.com>
2025-07-28 00:24:00 +0900

    feat: api 래핑 함수 구현

---
+++ frontend/src/apis/api.ts
@@ -0,0 +1,33 @@
+import { ApiConfig, apiRequest, ApiRequestOptions } from './fetcher';
+
+export const api = {
+  get: <T>(url: string, options: Omit<ApiRequestOptions<T>, 'method' | 'data'> = {}) =>
+    apiRequest<T, undefined>(url, { ...options, method: 'GET' }),
+
+  post: <T, TData>(
+    url: string,
+    data?: TData,
+    options: Omit<ApiRequestOptions<TData>, 'method' | 'data'> = {}
+  ) => apiRequest<T, TData>(url, { ...options, method: 'POST', data }),
+
+  put: <T, TData>(
+    url: string,
+    data?: TData,
+    options: Omit<ApiRequestOptions<TData>, 'method' | 'data'> = {}
+  ) => apiRequest<T, TData>(url, { ...options, method: 'PUT', data }),
+
+  patch: <T, TData>(
+    url: string,
+    data?: TData,
+    options: Omit<ApiRequestOptions<TData>, 'method' | 'data'> = {}
+  ) => apiRequest<T, TData>(url, { ...options, method: 'PATCH', data }),
+
+  delete: <T>(url: string, options: Omit<ApiRequestOptions<T>, 'method' | 'data'> = {}) =>
+    apiRequest<T, undefined>(url, { ...options, method: 'DELETE' }),
+
+  setDefaultConfig: (config: ApiConfig) => {
+    api.defaultConfig = { ...api.defaultConfig, ...config };
+  },
+
+  defaultConfig: {},
+};
