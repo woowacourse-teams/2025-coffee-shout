10eaec7e22c1d0e9539796ed9afebd2d1ace812c
sooyeoniya <tndus1306403@naver.com>
2025-07-27 15:07:48 +0900

    refactor: 텍스트 컴포넌트들 각 시맨틱 태그에 맞게 타입 수정

--- frontend/src/components/@common/Headline4/Headline4.tsx
+++ frontend/src/components/@common/Headline4/Headline4.tsx
@@ -1,8 +1,8 @@
 import { COLOR_MAP, ColorKey } from '@/constants/color';
-import { PropsWithChildren } from 'react';
+import { ComponentProps } from 'react';
 import * as S from './Headline4.styled';
 
-type Props = { color?: ColorKey } & PropsWithChildren;
+type Props = { color?: ColorKey } & ComponentProps<'h4'>;
 
 const Headline4 = ({ children, color = 'gray-700' as ColorKey }: Props) => {
   const resolvedColor = COLOR_MAP[color];
