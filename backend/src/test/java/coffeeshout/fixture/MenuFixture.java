40fba23efa54aa86b9740619b1b8d304af5352a8
MinJun Choi <mj043000@naver.com>
2025-07-28 17:58:52 +0900

    feat: MenuType Enum 추가 및 메뉴 관련 로직 수정 (#241)

--- backend/src/test/java/coffeeshout/fixture/MenuFixture.java
+++ backend/src/test/java/coffeeshout/fixture/MenuFixture.java
@@ -1,24 +1,25 @@
 package coffeeshout.fixture;
 
 import coffeeshout.room.domain.player.Menu;
+import coffeeshout.room.domain.player.MenuType;
 import org.springframework.test.util.ReflectionTestUtils;
 
 public class MenuFixture {
 
     public static Menu 아메리카노() {
-        final Menu 아메리카노 = new Menu("아메리카노", "americano.jpg");
+        final Menu 아메리카노 = new Menu("아메리카노", MenuType.COFFEE);
         ReflectionTestUtils.setField(아메리카노, "id", 1L);
         return 아메리카노;
     }
 
     public static Menu 라떼() {
-        final Menu 라떼 = new Menu("라떼", "latte.jpg");
+        final Menu 라떼 = new Menu("라떼", MenuType.COFFEE);
         ReflectionTestUtils.setField(라떼, "id", 2L);
         return 라떼;
     }
 
     public static Menu 아이스티() {
-        final Menu 아이스티 = new Menu("아이스티", "icetea.jpg");
+        final Menu 아이스티 = new Menu("아이스티", MenuType.ADE);
         ReflectionTestUtils.setField(아이스티, "id", 3L);
         return 아이스티;
     }
