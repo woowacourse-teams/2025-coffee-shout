4ee42541674109dd3a61f6a96de5db0c592cb49a
Ko Daeun <drechsu@gmail.com>
2025-07-28 15:06:29 +0900

    feat : 미니게임 선택 기능 구현 및 GUEST일떄 선택 불가능하게 변경

--- frontend/src/features/room/lobby/components/MiniGameSection/MiniGameSection.tsx
+++ frontend/src/features/room/lobby/components/MiniGameSection/MiniGameSection.tsx
@@ -1,14 +1,29 @@
 import GameActionButton from '@/components/@common/GameActionButton/GameActionButton';
 import SectionTitle from '@/components/@composition/SectionTitle/SectionTitle';
 import * as S from './MiniGameSection.styled';
+import { useState } from 'react';
 
 export const MiniGameSection = () => {
+  const [selectedGame, setSelectedGame] = useState<number>(-1);
+
+  const handleGameClick = (gameId: number) => {
+    setSelectedGame(gameId);
+  };
+
   return (
     <>
       <SectionTitle title="미니게임" description="미니게임을 선택해주세요" />
       <S.Wrapper>
-        <GameActionButton onClick={() => {}} isSelected={true} gameName="카드게임" />
-        <GameActionButton onClick={() => {}} isSelected={false} gameName="랜덤31" />
+        <GameActionButton
+          onClick={() => handleGameClick(0)}
+          isSelected={selectedGame === 0}
+          gameName="카드게임"
+        />
+        <GameActionButton
+          onClick={() => handleGameClick(1)}
+          isSelected={selectedGame === 1}
+          gameName="랜덤31"
+        />
       </S.Wrapper>
     </>
   );
