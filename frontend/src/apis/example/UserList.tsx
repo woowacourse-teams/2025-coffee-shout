d9402bdb0b158d307fe62035f222830460da52a7
SuHwa Lee <tnghk9611@naver.com>
2025-07-28 15:12:45 +0900

    [feat] 비동기 통신 구조 설계 (#226)
    * chore: 불필요한 파일 삭제
    
    * feat: 공통적으로 사용할 수 있는 fetcher 구현
    
    * feat: 커스텀 에러 객체 추가
    
    * feat: api 래핑 함수 구현
    
    * docs: 사용 예시 코드 및 사용법 문서 추가
    
    * chore: 콘솔 메세지 수정
    
    * refactor: 실제 요청보내는 함수 파일명 수정
    
    * docs: 문서 수정

---
+++ frontend/src/apis/example/UserList.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import useUsers from './useUser';
+
+const UserList = () => {
+  const { users, loading, error, fetchUsers } = useUsers();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩 중...</div>;
+  if (error) return <div>에러: {error}</div>;
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
