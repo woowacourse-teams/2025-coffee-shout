437de24e782f32f9c7186bc59bdfcbbcd30721d1
Ko Daeun <drechsu@gmail.com>
2025-07-29 11:10:19 +0900

    refactor : 시계방향으로 부채꼴 그리게 변경

--- frontend/src/features/roulette/utils/describeArc.ts
+++ frontend/src/features/roulette/utils/describeArc.ts
@@ -7,15 +7,15 @@ export const describeArc = (
   startAngle: number,
   endAngle: number
 ) => {
-  const start = polarToCartesian(cx, cy, r, endAngle);
-  const end = polarToCartesian(cx, cy, r, startAngle);
+  const start = polarToCartesian(cx, cy, r, startAngle);
+  const end = polarToCartesian(cx, cy, r, endAngle);
 
   const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
 
   return [
     `M ${cx} ${cy}`,
     `L ${start.x} ${start.y}`,
-    `A ${r} ${r} 0 ${largeArcFlag} 0 ${end.x} ${end.y}`,
+    `A ${r} ${r} 0 ${largeArcFlag} 1 ${end.x} ${end.y}`,
     'Z',
   ].join(' ');
 };
