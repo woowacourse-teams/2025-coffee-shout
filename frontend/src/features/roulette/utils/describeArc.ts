25e3c2b778e2dd74f926f696851070f8ec2c569b
Ko Daeun <drechsu@gmail.com>
2025-07-30 13:07:56 +0900

    refactor : 파라미터 객체로 넘기게 수정

--- frontend/src/features/roulette/utils/describeArc.ts
+++ frontend/src/features/roulette/utils/describeArc.ts
@@ -1,14 +1,16 @@
 import { polarToCartesian } from './polarToCartesian';
 
-export const describeArc = (
-  cx: number,
-  cy: number,
-  r: number,
-  startAngle: number,
-  endAngle: number
-) => {
-  const start = polarToCartesian(cx, cy, r, startAngle);
-  const end = polarToCartesian(cx, cy, r, endAngle);
+type Props = {
+  cx: number;
+  cy: number;
+  r: number;
+  startAngle: number;
+  endAngle: number;
+};
+
+export const describeArc = ({ cx, cy, r, startAngle, endAngle }: Props) => {
+  const start = polarToCartesian({ cx, cy, r, angle: startAngle });
+  const end = polarToCartesian({ cx, cy, r, angle: endAngle });
 
   const largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
 
--- frontend/src/features/roulette/utils/polarToCartesian.ts
+++ frontend/src/features/roulette/utils/polarToCartesian.ts
@@ -1,4 +1,11 @@
-export const polarToCartesian = (cx: number, cy: number, r: number, angle: number) => {
+type Props = {
+  cx: number;
+  cy: number;
+  r: number;
+  angle: number;
+};
+
+export const polarToCartesian = ({ cx, cy, r, angle }: Props) => {
   const rad = (angle - 90) * (Math.PI / 180); // 12시 방향이 0도 기준
   return {
     x: cx + r * Math.cos(rad),
