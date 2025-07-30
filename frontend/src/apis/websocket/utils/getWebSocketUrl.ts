0a33c0c1c1d0d2152a791ab17ecbd2b51a36cd6f
Ko Daeun <drechsu@gmail.com>
2025-07-30 11:26:27 +0900

    refactor : 에러 메세지 함수로 나음

--- frontend/src/apis/websocket/utils/getWebSocketUrl.ts
+++ frontend/src/apis/websocket/utils/getWebSocketUrl.ts
@@ -1,7 +1,7 @@
 export const getWebSocketUrl = (): string => {
   const apiUrl = process.env.REACT_APP_API_URL;
   if (!apiUrl) {
-    throw new Error('REACT_APP_API_URL is not defined');
+    throw new Error('REACT_APP_API_URL가 정의되지 않았습니다');
   }
 
   return apiUrl + '/ws';
