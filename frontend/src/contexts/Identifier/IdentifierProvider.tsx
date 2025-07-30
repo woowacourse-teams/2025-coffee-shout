c791bacd502ceb0e1955f4b3570e7a4d446fc4ec
sooyeoniya <tndus1306403@naver.com>
2025-07-30 14:56:27 +0900

    feat: 메인 페이지 상태 초기화

--- frontend/src/contexts/Identifier/IdentifierProvider.tsx
+++ frontend/src/contexts/Identifier/IdentifierProvider.tsx
@@ -1,17 +1,17 @@
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren, useCallback, useState } from 'react';
 import { IdentifierContext } from './IdentifierContext';
 
 export const IdentifierProvider = ({ children }: PropsWithChildren) => {
   const [joinCode, setJoinCode] = useState<string>('');
   const [myName, setMyName] = useState<string>('');
 
-  const clearJoinCode = () => {
+  const clearJoinCode = useCallback(() => {
     setJoinCode('');
-  };
+  }, []);
 
-  const clearMyName = () => {
+  const clearMyName = useCallback(() => {
     setMyName('');
-  };
+  }, []);
 
   return (
     <IdentifierContext.Provider
