01299c1a1de6e51a66d4cd64899e8c5c857108de
sooyeoniya <tndus1306403@naver.com>
2025-07-30 17:27:11 +0900

    feat: 미니게임 선택 웹소켓 연동

--- frontend/src/features/room/lobby/pages/LobbyPage.tsx
+++ frontend/src/features/room/lobby/pages/LobbyPage.tsx
@@ -1,11 +1,14 @@
+import { useWebSocket } from '@/apis/websocket/contexts/WebSocketContext';
+import { useWebSocketSubscription } from '@/apis/websocket/hooks/useWebSocketSubscription';
 import ShareIcon from '@/assets/share-icon.svg';
 import BackButton from '@/components/@common/BackButton/BackButton';
 import Button from '@/components/@common/Button/Button';
 import useModal from '@/components/@common/Modal/useModal';
 import ToggleButton from '@/components/@common/ToggleButton/ToggleButton';
+import { useIdentifier } from '@/contexts/Identifier/IdentifierContext';
 import { usePlayerType } from '@/contexts/PlayerType/PlayerTypeContext';
 import Layout from '@/layouts/Layout';
-import { ReactElement, useState } from 'react';
+import { ReactElement, useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import JoinCodeModal from '../components/JoinCodeModal/JoinCodeModal';
 import { MiniGameSection } from '../components/MiniGameSection/MiniGameSection';
@@ -13,25 +16,24 @@ import { ParticipantSection } from '../components/ParticipantSection/Participant
 import { RouletteSection } from '../components/RouletteSection/RouletteSection';
 import * as S from './LobbyPage.styled';
 
-type SectionType = '참가자' | '룰렛' | '미니게임';
-type SectionComponents = {
-  [K in SectionType]: ReactElement;
-};
-
-const SECTIONS: SectionComponents = {
-  참가자: <ParticipantSection />,
-  룰렛: <RouletteSection />,
-  미니게임: <MiniGameSection />,
-} as const;
+export type SectionType = '참가자' | '룰렛' | '미니게임';
+type SectionComponents = Record<SectionType, ReactElement>;
+type MiniGameResponse = string[];
 
 const LobbyPage = () => {
   const navigate = useNavigate();
   const { openModal } = useModal();
   const { playerType } = usePlayerType();
+  const { joinCode } = useIdentifier();
+  const { send } = useWebSocket();
   const [currentSection, setCurrentSection] = useState<SectionType>('참가자');
+  const [selectedMiniGames, setSelectedMiniGames] = useState<string[]>([]);
 
-  //TODO: 다른 에러 처리방식을 찾아보기
-  if (!playerType) return null;
+  const handleMiniGameData = useCallback((data: MiniGameResponse) => {
+    setSelectedMiniGames(data);
+  }, []);
+
+  useWebSocketSubscription<MiniGameResponse>(`/room/${joinCode}/minigame`, handleMiniGameData);
 
   const handleClickBackButton = () => {
     navigate(-1);
@@ -51,6 +53,37 @@ const LobbyPage = () => {
       showCloseButton: true,
     });
   };
+
+  const handleMiniGameClick = (miniGameType: string) => {
+    if (playerType === 'GUEST') return;
+
+    const updatedMiniGames = selectedMiniGames.includes(miniGameType)
+      ? selectedMiniGames.filter((game) => game !== miniGameType)
+      : [...selectedMiniGames, miniGameType];
+
+    setSelectedMiniGames(updatedMiniGames);
+
+    send(
+      `/room/${joinCode}/select-minigames`,
+      JSON.stringify({
+        hostName: playerType,
+        miniGameType: updatedMiniGames,
+      })
+    );
+  };
+
+  const SECTIONS: SectionComponents = {
+    참가자: <ParticipantSection />,
+    룰렛: <RouletteSection />,
+    미니게임: (
+      <MiniGameSection
+        selectedMiniGames={selectedMiniGames}
+        handleMiniGameClick={handleMiniGameClick}
+      />
+    ),
+  };
+
+  if (!playerType) return null;
 
   return (
     <Layout>
