cf5c8e4e342ad4b3b91d1e791f443ecd7ff46b41
SuHwa Lee <tnghk9611@naver.com>
2025-07-30 14:41:18 +0900

    refactor: data를 body로 변경

--- frontend/src/apis/rest/api.ts
+++ frontend/src/apis/rest/api.ts
@@ -2,28 +2,28 @@ import { ApiConfig, apiRequest, ApiRequestOptions } from './apiRequest';
 
 export const api = {
   get: <T>(url: string, options: Omit<ApiRequestOptions<T>, 'method' | 'data'> = {}) =>
-    apiRequest<T, undefined>(url, { ...options, method: 'GET' }),
+    apiRequest<T, undefined>(url, { ...options, method: 'GET', body: undefined }),
 
   post: <T, TData>(
     url: string,
     data?: TData,
     options: Omit<ApiRequestOptions<TData>, 'method' | 'data'> = {}
-  ) => apiRequest<T, TData>(url, { ...options, method: 'POST', data }),
+  ) => apiRequest<T, TData>(url, { ...options, method: 'POST', body: data }),
 
   put: <T, TData>(
     url: string,
     data?: TData,
     options: Omit<ApiRequestOptions<TData>, 'method' | 'data'> = {}
-  ) => apiRequest<T, TData>(url, { ...options, method: 'PUT', data }),
+  ) => apiRequest<T, TData>(url, { ...options, method: 'PUT', body: data }),
 
   patch: <T, TData>(
     url: string,
     data?: TData,
     options: Omit<ApiRequestOptions<TData>, 'method' | 'data'> = {}
-  ) => apiRequest<T, TData>(url, { ...options, method: 'PATCH', data }),
+  ) => apiRequest<T, TData>(url, { ...options, method: 'PATCH', body: data }),
 
   delete: <T>(url: string, options: Omit<ApiRequestOptions<T>, 'method' | 'data'> = {}) =>
-    apiRequest<T, undefined>(url, { ...options, method: 'DELETE' }),
+    apiRequest<T, undefined>(url, { ...options, method: 'DELETE', body: undefined }),
 
   setDefaultConfig: (config: ApiConfig) => {
     api.defaultConfig = { ...api.defaultConfig, ...config };
