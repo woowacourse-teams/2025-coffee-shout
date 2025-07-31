830f2e6767f735aef3d039195bfe79bad7239b46
HyeonsuLee <leehyeonsu4888@naver.com>
2025-07-31 11:19:49 +0900

    feat: heart beat 설정 추가

--- backend/src/main/java/coffeeshout/global/config/WebSocketConfig.java
+++ backend/src/main/java/coffeeshout/global/config/WebSocketConfig.java
@@ -1,19 +1,27 @@
 package coffeeshout.global.config;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@AllArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final TaskScheduler taskScheduler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic/", "/queue/");
+        config.enableSimpleBroker("/topic/", "/queue/")
+                .setHeartbeatValue(new long[]{4000, 4000})
+                .setTaskScheduler(taskScheduler);
+
         config.setApplicationDestinationPrefixes("/app");
     }
 
---
+++ backend/src/main/java/coffeeshout/global/config/WebSocketSchedulerConfig.java
@@ -0,0 +1,21 @@
+package coffeeshout.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class WebSocketSchedulerConfig {
+
+    @Bean
+    @Primary
+    public TaskScheduler heartBeatMessageBrokerTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
