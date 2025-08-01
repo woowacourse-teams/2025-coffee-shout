9ce9817f1a4e5d97f33c1e1b96238c1432c454f6
7i.w__k <tizmfns3@gmail.com>
2025-08-01 16:38:36 +0900

    feat: add new card game task types and improve task chaining logic

--- backend/src/main/java/coffeeshout/minigame/domain/temp/ChainedTaskV2.java
+++ backend/src/main/java/coffeeshout/minigame/domain/temp/ChainedTaskV2.java
@@ -7,39 +7,41 @@ import java.util.concurrent.ScheduledFuture;
 import org.springframework.scheduling.TaskScheduler;
 
 public class ChainedTaskV2  {
+
     private final Runnable action;
     private final Duration preDelay;
     private final Duration postDelay;
     private ScheduledFuture<?> future;
-    private ChainedTask nextChainedTask;
+    private ChainedTaskV2 previousChainedTask;
+    private ChainedTaskV2 nextChainedTask;
 
     private ChainedTaskV2(Builder builder) {
         this.action = builder.action;
-        this.preDelay = builder.preDelay;
-        this.postDelay = builder.postDelay;
+        this.preDelay = builder.preDelay == null ? Duration.ZERO : builder.preDelay;
+        this.postDelay = builder.postDelay == null ? Duration.ZERO : builder.postDelay;
     }
 
-    public void start(TaskScheduler scheduler) {
-        Instant startTime = Instant.now().plus(preDelay);
+    public void start(final TaskScheduler scheduler) {
+        final Instant startTime = getStartTime();
 
         future = scheduler.schedule(() -> {
-            action.run(); // 작업 실행
-
+            action.run();
             if (nextChainedTask != null) {
-                // 실행 후 지연 적용
-                Instant nextStartTime = postDelay.isZero() ?
-                        Instant.now() : Instant.now().plus(postDelay);
-
-                scheduler.schedule(
-                        () -> nextChainedTask.start(scheduler),
-                        nextStartTime
-                );
+                nextChainedTask.start(scheduler);
             }
         }, startTime);
     }
 
-    public void setNextTask(ChainedTask nextChainedTask) {
+    private Instant getStartTime() {
+        if (previousChainedTask != null) {
+            return Instant.now().plus(preDelay).plus(previousChainedTask.postDelay);
+        }
+        return Instant.now().plus(preDelay);
+    }
+
+    public void setNextTask(ChainedTaskV2 nextChainedTask) {
         this.nextChainedTask = nextChainedTask;
+        nextChainedTask.previousChainedTask = this;
     }
 
     public void join() throws ExecutionException, InterruptedException {
@@ -70,7 +72,11 @@ public class ChainedTaskV2  {
         return future != null;
     }
 
-    private static class Builder {
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
         private Runnable action;
         private Duration preDelay = Duration.ZERO;
         private Duration postDelay = Duration.ZERO;
