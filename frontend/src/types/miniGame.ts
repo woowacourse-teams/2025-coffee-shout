5f12fe3d807df900441f323902b7482b33e953b2
Ko Daeun <drechsu@gmail.com>
2025-08-01 17:54:53 +0900

    docs : 주석 수정

--- frontend/src/types/miniGame.ts
+++ frontend/src/types/miniGame.ts
@@ -1,6 +1,6 @@
 export const MINI_GAME_NAME_MAP = {
   CARD_GAME: '카드게임',
-  // '31_GAME': '랜덤 31',
+  // '31_GAME': '랜덤 31222',
 } as const;
 
 export type MiniGameType = keyof typeof MINI_GAME_NAME_MAP;
