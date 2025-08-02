70e1cb87f6034af59cd6fa2f471b4146e15018c2
SuHwa Lee <tnghk9611@naver.com>
2025-08-02 16:47:15 +0900

    fix: ParticipantResponse 타입을 export로 변경

--- frontend/src/features/room/lobby/pages/LobbyPage.tsx
+++ frontend/src/features/room/lobby/pages/LobbyPage.tsx
@@ -23,7 +23,7 @@ import * as S from './LobbyPage.styled';
 type SectionType = '참가자' | '룰렛' | '미니게임';
 type SectionComponents = Record<SectionType, ReactElement>;
 
-type ParticipantResponse = Player[];
+export type ParticipantResponse = Player[];
 
 const LobbyPage = () => {
   const navigate = useNavigate();
