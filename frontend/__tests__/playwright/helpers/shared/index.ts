559636780904624aa1652cd6b420596a82aa1486
sooyeoniya <tndus1306403@naver.com>
2025-08-06 00:10:57 +0900

    refactor: BaseButtonHelper 클래스를 ButtonHelper로 통합 및 불필요한 파일 삭제
    - BaseButtonHelper 클래스를 ButtonHelper로 통합하여 코드 중복 제거
    - BaseButtonHelper.ts 파일 삭제로 프로젝트 구조 간소화

--- frontend/__tests__/playwright/helpers/shared/index.ts
+++ frontend/__tests__/playwright/helpers/shared/index.ts
@@ -1,4 +1,3 @@
-export { BaseButtonHelper } from './BaseButtonHelper';
 export { ButtonHelper } from './ButtonHelper';
 export { FormHelper } from './FormHelper';
 export { NavigationHelper } from './NavigationHelper';
