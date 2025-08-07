6fa0c0417fce547aba4a6378792538f51431c6b3
sooyeoniya <tndus1306403@naver.com>
2025-08-07 13:50:53 +0900

    chore: environment 속성 주석 처리

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ import * as Sentry from '@sentry/react';
 Sentry.init({
   dsn: 'https://b2afc6b0a990c07e81f7458240917d0f@o4509780188987392.ingest.us.sentry.io/4509784133271552',
   release: process.env.REACT_APP_VERSION || '1.0.0',
-  environment: 'production', // process.env.NODE_ENV || 'development'
+  // environment: process.env.NODE_ENV || 'development'
   sendDefaultPii: true,
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   replaysSessionSampleRate: 0.1,
