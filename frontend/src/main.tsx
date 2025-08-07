4d2cb20ad24a4eb7dcdf8ec2a0d132ef510a1d50
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-08-07 13:51:32 +0900

    [chore] Sentry 초기화 코드 수정 및 환경변수 기본값 설정 (#433)
    * refactor: Sentry 초기화 코드 수정 및 환경 변수 기본값 설정
    
    * chore: environment 속성 주석 처리

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,19 +6,19 @@ import './styles/global.css';
 import './styles/reset.css';
 import * as Sentry from '@sentry/react';
 
-if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://b2afc6b0a990c07e81f7458240917d0f@o4509780188987392.ingest.us.sentry.io/4509784133271552',
-    release: process.env.REACT_APP_VERSION,
-    environment: process.env.NODE_ENV,
-    sendDefaultPii: true,
-    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-    tracesSampleRate: 0.2,
-    tracePropagationTargets: ['localhost:8080', 'https://api.coffee-shout.com', /^\/api\//],
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+Sentry.init({
+  dsn: 'https://b2afc6b0a990c07e81f7458240917d0f@o4509780188987392.ingest.us.sentry.io/4509784133271552',
+  release: process.env.REACT_APP_VERSION || '1.0.0',
+  // environment: process.env.NODE_ENV || 'development'
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  tracesSampleRate: 0.2,
+  tracePropagationTargets: ['localhost:8080', 'https://api.coffee-shout.com', /^\/api\//],
+});
+// }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
