d597ab53915439edf61665eeee6799e023ade411
Ko Daeun <drechsu@gmail.com>
2025-08-11 16:33:15 +0900

    refactgor : CONFIG 상수 분리

--- frontend/src/features/roulette/components/constants/config.ts
+++ frontend/src/features/roulette/components/constants/config.ts
@@ -5,3 +5,10 @@ export const WHEEL_CONFIG = {
   TEXT_RADIUS: 80,
   STROKE_WIDTH: 1,
 } as const;
+
+export const SVG_CONFIG = {
+  SIZE: 300,
+  CENTER: 150,
+  RADIUS: 140,
+  STROKE_WIDTH: 1,
+} as const;
