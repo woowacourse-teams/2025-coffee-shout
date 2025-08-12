2d6e234d454072b27e52cf3802446d7736e0e789
sooyeoniya <tndus1306403@naver.com>
2025-08-12 15:06:26 +0900

    feat: EntryMenuPage에서 setMenuId 추가하여 메뉴 ID 관리 기능 개선

--- frontend/src/features/entry/pages/EntryMenuPage/EntryMenuPage.tsx
+++ frontend/src/features/entry/pages/EntryMenuPage/EntryMenuPage.tsx
@@ -42,7 +42,7 @@ const EntryMenuPage = () => {
   const navigate = useNavigate();
   const { startSocket, isConnected } = useWebSocket();
   const { playerType } = usePlayerType();
-  const { joinCode, myName, setJoinCode } = useIdentifier();
+  const { joinCode, myName, setJoinCode, setMenuId } = useIdentifier();
   const [selectedValue, setSelectedValue] = useState<Option>({ id: -1, name: '' });
   const [coffeeOptions, setCoffeeOptions] = useState<Option[]>([]);
   const [loading, setLoading] = useState(true);
@@ -102,6 +102,7 @@ const EntryMenuPage = () => {
         menuId,
       });
       setJoinCode(joinCode);
+      setMenuId(menuId);
       startSocket(joinCode, myName, menuId);
     };
 
@@ -115,6 +116,7 @@ const EntryMenuPage = () => {
         }
       );
       setJoinCode(_joinCode);
+      setMenuId(menuId);
       startSocket(_joinCode, myName, menuId);
     };
 
