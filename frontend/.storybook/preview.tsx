68bb657d4d2cf21382bad7bf6f5cd96dd7d0278d
SuHwa Lee <tnghk9611@naver.com>
2025-08-12 20:26:31 +0900

    chore: storybook에 provider 설정

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,17 +1,24 @@
+import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import type { Preview } from '@storybook/react-webpack5';
 import { ModalProvider } from '../src/components/@common/Modal/ModalContext';
+import { IdentifierProvider } from '../src/contexts/Identifier/IdentifierProvider';
 import '../src/styles/global.css';
 import '../src/styles/reset.css';
 import { theme } from '../src/styles/theme';
+import { MemoryRouter } from 'react-router-dom';
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <ThemeProvider theme={theme}>
-        <ModalProvider>
-          <Story />
-        </ModalProvider>
+        <MemoryRouter>
+          <IdentifierProvider>
+            <ModalProvider>
+              <Story />
+            </ModalProvider>
+          </IdentifierProvider>
+        </MemoryRouter>
       </ThemeProvider>
     ),
   ],
