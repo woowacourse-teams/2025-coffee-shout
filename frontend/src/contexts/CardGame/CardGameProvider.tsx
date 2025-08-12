199cba68ac5e6e816a91c1d849c31c68ec8c0068
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-08-12 12:59:03 +0900

    [fix]  아무것도 안눌렀을 때 랜덤 카드가 보이지 않는 문제 해결 (#439)
    * refactor : api 주소 변경
    
    * feat : 라운드 매핑 상수
    
    * fix :아무것도 안눌렀을 때 랜덤 카드가 보이지 않는 문제 해결
    
    * refactor : 불필요한 의존성 제거
    
    * refactor : react impot 컨벤션 수정
    
    * feat : 1라운드 playing일떄 상태 Playing으로 변경
    
    * refactor : handleScoreBoard 함수 만듬, 이미 카드를 뽑았을경우 setSelectedCardInfo 하지 않음
    
    * refactor: CardGameRound 타입을 constants로 이동하고 import 업데이트하기
    
    * refactor: CardGameProvider에서 isTransition 상태의 타입을 명시적으로 설정
    
    * refactor: SelectedCardInfo 타입을 miniGame.ts에 추가하고 관련 import 업데이트
    
    * refactor: 중복상태 선언 제거
    
    * fix : 빌드오류 수정

--- frontend/src/contexts/CardGame/CardGameProvider.tsx
+++ frontend/src/contexts/CardGame/CardGameProvider.tsx
@@ -2,39 +2,77 @@ import { PropsWithChildren, useCallback, useState } from 'react';
 import { CardGameContext } from './CardGameContext';
 import { useWebSocketSubscription } from '@/apis/websocket/hooks/useWebSocketSubscription';
 import { useIdentifier } from '../Identifier/IdentifierContext';
-import { CardGameState, CardGameStateData, CardInfo } from '@/types/miniGame';
-import { RoundKey } from '@/types/round';
+import { CardGameState, CardGameStateData, CardInfo, SelectedCardInfo } from '@/types/miniGame';
+
 import { useNavigate, useParams } from 'react-router-dom';
+import { CardGameRound } from '@/constants/miniGame';
 
 const CardGameProvider = ({ children }: PropsWithChildren) => {
   const navigate = useNavigate();
-  const { joinCode } = useIdentifier();
+  const { joinCode, myName } = useIdentifier();
   const { miniGameType } = useParams();
   const [startCardGame, setStartCardGame] = useState<boolean>(false);
-  const [isTransition, setIsTransition] = useState(false);
-  const [currentRound, setCurrentRound] = useState<RoundKey>(1);
+  const [isTransition, setIsTransition] = useState<boolean>(false);
+  const [currentRound, setCurrentRound] = useState<CardGameRound>('FIRST');
   const [currentCardGameState, setCurrentCardGameState] = useState<CardGameState>('READY');
   const [cardInfos, setCardInfos] = useState<CardInfo[]>([]);
+  const [selectedCardInfo, setSelectedCardInfo] = useState<SelectedCardInfo>({
+    FIRST: {
+      isSelected: false,
+      type: null,
+      value: null,
+    },
+    SECOND: {
+      isSelected: false,
+      type: null,
+      value: null,
+    },
+  });
 
   const handleCardGameState = useCallback(
     (data: CardGameStateData) => {
       const { cardGameState, currentRound, cardInfoMessages } = data;
 
       const isFirstRoundPlaying = cardGameState === 'PLAYING' && currentRound === 'FIRST';
+      const isFirstRoundScoreBoard = cardGameState === 'SCORE_BOARD' && currentRound === 'FIRST';
       const isSecondRoundLoading = cardGameState === 'LOADING' && currentRound === 'SECOND';
       const isSecondRoundPlaying = cardGameState === 'PLAYING' && currentRound === 'SECOND';
       const isSecondRoundScoreBoard = cardGameState === 'SCORE_BOARD' && currentRound === 'SECOND';
       const isGameDone = cardGameState === 'DONE';
 
+      const handleScoreBoard = () => {
+        setCurrentCardGameState('SCORE_BOARD');
+        setCardInfos(cardInfoMessages);
+
+        const mySelectedCardInfo = cardInfoMessages.find((card) => card.playerName === myName);
+        if (!mySelectedCardInfo) return;
+        if (selectedCardInfo[currentRound].isSelected) return;
+
+        setSelectedCardInfo((prev) => ({
+          ...prev,
+          [currentRound]: {
+            isSelected: true,
+            type: mySelectedCardInfo.cardType,
+            value: mySelectedCardInfo.value,
+          },
+        }));
+      };
+
       if (isFirstRoundPlaying) {
         setStartCardGame(true);
+        setCurrentCardGameState('PLAYING');
         setCardInfos(cardInfoMessages);
+        return;
+      }
+
+      if (isFirstRoundScoreBoard) {
+        handleScoreBoard();
         return;
       }
 
       if (isSecondRoundLoading) {
         setIsTransition(true);
-        setCurrentRound(2);
+        setCurrentRound('SECOND');
         setCurrentCardGameState('LOADING');
         return;
       }
@@ -45,16 +83,18 @@ const CardGameProvider = ({ children }: PropsWithChildren) => {
         setCurrentCardGameState('PLAYING');
         return;
       }
+
       if (isSecondRoundScoreBoard) {
-        setCurrentCardGameState('SCORE_BOARD');
+        handleScoreBoard();
         return;
       }
+
       if (isGameDone) {
         navigate(`/room/${joinCode}/${miniGameType}/result`);
         return;
       }
     },
-    [navigate, joinCode, miniGameType]
+    [navigate, joinCode, miniGameType, myName, setSelectedCardInfo, selectedCardInfo]
   );
 
   useWebSocketSubscription(`/room/${joinCode}/gameState`, handleCardGameState);
@@ -67,6 +107,8 @@ const CardGameProvider = ({ children }: PropsWithChildren) => {
         currentRound,
         currentCardGameState,
         cardInfos,
+        selectedCardInfo,
+        setSelectedCardInfo,
       }}
     >
       {children}
