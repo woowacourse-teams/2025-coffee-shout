12df594f05becbd67c329ffd618ab12185013000
SuHwa Lee <tnghk9611@naver.com>
2025-08-13 14:19:44 +0900

    feat: storybook에 Provider 추가

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from '@emotion/react';
 import type { Preview } from '@storybook/react-webpack5';
 import { ModalProvider } from '../src/components/@common/Modal/ModalContext';
 import { IdentifierProvider } from '../src/contexts/Identifier/IdentifierProvider';
+import CardGameProvider from '../src/contexts/CardGame/CardGameProvider';
+import { WebSocketProvider } from '../src/apis/websocket/contexts/WebSocketProvider';
 import '../src/styles/global.css';
 import '../src/styles/reset.css';
 import { theme } from '../src/styles/theme';
@@ -12,13 +14,17 @@ const preview: Preview = {
   decorators: [
     (Story) => (
       <ThemeProvider theme={theme}>
-        <MemoryRouter>
-          <IdentifierProvider>
-            <ModalProvider>
-              <Story />
-            </ModalProvider>
-          </IdentifierProvider>
-        </MemoryRouter>
+        <WebSocketProvider>
+          <MemoryRouter>
+            <IdentifierProvider>
+              <CardGameProvider>
+                <ModalProvider>
+                  <Story />
+                </ModalProvider>
+              </CardGameProvider>
+            </IdentifierProvider>
+          </MemoryRouter>
+        </WebSocketProvider>
       </ThemeProvider>
     ),
   ],
