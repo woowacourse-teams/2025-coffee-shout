72ee00e15fbaf804349273ddcb37baca9a35cd29
7i.w__k <tizmfns3@gmail.com>
2025-08-16 21:19:47 +0900

    refactor: 불필요한 MockitoExtenstion 제거

--- backend/src/test/java/coffeeshout/global/websocket/StompSessionManagerTest.java
+++ backend/src/test/java/coffeeshout/global/websocket/StompSessionManagerTest.java
@@ -6,10 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class StompSessionManagerTest {
 
     StompSessionManager sessionManager;
