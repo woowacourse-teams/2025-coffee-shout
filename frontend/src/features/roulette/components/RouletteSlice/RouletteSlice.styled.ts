aa862817f99464b7068506398893e51418b4223d
sooyeoniya <tndus1306403@naver.com>
2025-08-18 20:55:11 +0900

    design: PlayerNameText의 fill 색상을 흰색으로 변경

--- frontend/src/features/roulette/components/RouletteSlice/RouletteSlice.styled.ts
+++ frontend/src/features/roulette/components/RouletteSlice/RouletteSlice.styled.ts
@@ -2,7 +2,7 @@ import { DESIGN_TOKENS } from '@/constants/design';
 import styled from '@emotion/styled';
 
 export const PlayerNameText = styled.text`
-  fill: ${({ theme }) => theme.color.point[100]};
+  fill: ${({ theme }) => theme.color.white};
   font-size: ${DESIGN_TOKENS.typography.small};
   font-weight: bold;
 `;
