f854f243769ffd20ab6f3f396f57b702155793c3
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-08-19 17:13:17 +0900

    [feat] Toast 컴포넌트 구현 (#565)
    * feat: 404 페이지 스타일 및 레이아웃 추가
    
    * fix: Storybook의 Provider 순서 변경
    
    WebSocketProvider 내부에서 IdentifierProvider 를 사용하고 있어서 발생하는 문제 해결
    
    * style: Modal 백드롭 불필요한 스타일 속성 제거
    
    * refactor: zIndex에 TOAST 상수 추가
    
    MODAL의 z-index 를 999로 변경
    TOAST가 MODAL보다 쌓임 맥락에서 위에 존재해야 함
    
    * feat: Toast 컴포넌트 구현
    
    * chore: 안쓰는 아이콘 삭제
    
    * feat: Toast 컴포넌트에 아이콘 추가 및 애니메이션 효과 구현
    
    - Toast 컴포넌트에 성공, 오류, 경고, 정보 아이콘 추가
    - Toast 애니메이션 효과 (슬라이드 인/아웃) 구현
    - Toast 색상 및 스타일 개선
    
    * feat: Toast 컴포넌트 Storybook 스토리 구현
    
    * chore: type 컨벤션 수정
    
    * refactor: ToastContext 리팩토링

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import { ThemeProvider } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 import { WebSocketProvider } from './apis/websocket/contexts/WebSocketProvider';
 import { ModalProvider } from './components/@common/Modal/ModalContext';
+import { ToastProvider } from './components/@common/Toast/ToastContext';
 import { IdentifierProvider } from './contexts/Identifier/IdentifierProvider';
-import { theme } from './styles/theme';
+import { ParticipantsProvider } from './contexts/Participants/ParticipantsProvider';
 import { PlayerTypeProvider } from './contexts/PlayerType/PlayerTypeProvider';
 import ProbabilityHistoryProvider from './contexts/ProbabilityHistory/ProbabilityHistoryProvider';
-import { ParticipantsProvider } from './contexts/Participants/ParticipantsProvider';
+import { theme } from './styles/theme';
 
 const App = () => {
   return (
@@ -16,9 +17,11 @@ const App = () => {
           <ParticipantsProvider>
             <PlayerTypeProvider>
               <ProbabilityHistoryProvider>
-                <ModalProvider>
-                  <Outlet />
-                </ModalProvider>
+                <ToastProvider>
+                  <ModalProvider>
+                    <Outlet />
+                  </ModalProvider>
+                </ToastProvider>
               </ProbabilityHistoryProvider>
             </PlayerTypeProvider>
           </ParticipantsProvider>
