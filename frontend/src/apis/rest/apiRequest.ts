492ac3ee2c567057635cb551a46180fcc6a4d484
최수연 (SooYeon Choi) <87463004+sooyeoniya@users.noreply.github.com>
2025-08-19 11:57:31 +0900

    [chore] 환경변수 수정 (#572)
    * chore: 환경변수 수정
    
    * chore: buildspec 불필요한 내용 삭제

--- frontend/src/apis/rest/apiRequest.ts
+++ frontend/src/apis/rest/apiRequest.ts
@@ -1,7 +1,7 @@
 import { ApiError, NetworkError } from './error';
 import { reportApiError } from '@/apis/utils/reportSentryError';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://api.dev.coffee-shout.com';
+const API_URL = process.env.API_URL;
 
 type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
