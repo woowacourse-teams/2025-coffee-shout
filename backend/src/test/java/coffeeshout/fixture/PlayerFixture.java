990bd0e67e1774d4438a0a41294b602d61e6db1f
HyeonsuLee <leehyeonsu4888@naver.com>
2025-08-21 12:24:09 +0900

    chore: 테스트에서만 사용하는 createHost, createGuest제거

--- backend/src/test/java/coffeeshout/fixture/PlayerFixture.java
+++ backend/src/test/java/coffeeshout/fixture/PlayerFixture.java
@@ -9,41 +9,41 @@ public final class PlayerFixture {
     }
 
     public static Player 호스트한스() {
-        return Player.createHost(new PlayerName("한스"),  MenuFixture.아메리카노(), null);
+        return Player.createHost(new PlayerName("한스"),  MenuFixture.아메리카노());
     }
 
     public static Player 호스트꾹이() {
-        return Player.createHost(new PlayerName("꾹이"), MenuFixture.아메리카노(), null);
+        return Player.createHost(new PlayerName("꾹이"), MenuFixture.아메리카노());
     }
 
     public static Player 호스트루키() {
-        return Player.createHost(new PlayerName("루키"),  MenuFixture.아메리카노(), null);
+        return Player.createHost(new PlayerName("루키"),  MenuFixture.아메리카노());
     }
 
     public static Player 호스트엠제이() {
-        return Player.createHost(new PlayerName("엠제이"),  MenuFixture.아메리카노(), null);
+        return Player.createHost(new PlayerName("엠제이"),  MenuFixture.아메리카노());
     }
 
     public static Player 게스트한스() {
-        final Player 한스 = Player.createGuest(new PlayerName("한스"),  MenuFixture.아메리카노(), null);
+        final Player 한스 = Player.createGuest(new PlayerName("한스"),  MenuFixture.아메리카노());
         한스.updateReadyState(true);
         return 한스;
     }
 
     public static Player 게스트꾹이() {
-        final Player 꾹이 = Player.createGuest(new PlayerName("꾹이"),  MenuFixture.아메리카노(), null);
+        final Player 꾹이 = Player.createGuest(new PlayerName("꾹이"),  MenuFixture.아메리카노());
         꾹이.updateReadyState(true);
         return 꾹이;
     }
 
     public static Player 게스트루키() {
-        final Player 루키 = Player.createGuest(new PlayerName("루키"),  MenuFixture.아메리카노(), null);
+        final Player 루키 = Player.createGuest(new PlayerName("루키"),  MenuFixture.아메리카노());
         루키.updateReadyState(true);
         return 루키;
     }
 
     public static Player 게스트엠제이() {
-        final Player 엠제이 = Player.createGuest(new PlayerName("엠제이"),  MenuFixture.아메리카노(), null);
+        final Player 엠제이 = Player.createGuest(new PlayerName("엠제이"),  MenuFixture.아메리카노());
         엠제이.updateReadyState(true);
         return 엠제이;
     }
