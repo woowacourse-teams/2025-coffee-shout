0ab5b50544dfed6b341f39901c19ee90b6580fdf
Ko Daeun <drechsu@gmail.com>
2025-09-04 13:54:25 +0900

    refactor : TemperatureToggle 버튼 확장성 있게 리팩터링

--- frontend/src/components/@common/TemperatureToggle/TemperatureToggle.tsx
+++ frontend/src/components/@common/TemperatureToggle/TemperatureToggle.tsx
@@ -7,21 +7,20 @@ type Props = {
   onClick: (option: TemperatureOption) => void;
 };
 
+const TEMPERATURE_OPTIONS: TemperatureOption[] = ['ICED', 'HOT'];
+
 const TemperatureToggle = ({ selectedTemperature, onClick }: Props) => {
   return (
     <S.Container>
-      <TemperatureOptionButton
-        option="ICED"
-        position="left"
-        selected={selectedTemperature === 'ICED'}
-        onClick={() => onClick('ICED')}
-      />
-      <TemperatureOptionButton
-        option="HOT"
-        position="right"
-        selected={selectedTemperature === 'HOT'}
-        onClick={() => onClick('HOT')}
-      />
+      {TEMPERATURE_OPTIONS.map((option, index) => (
+        <TemperatureOptionButton
+          key={option}
+          option={option}
+          position={index === 0 ? 'left' : 'right'}
+          selected={selectedTemperature === option}
+          onClick={() => onClick(option)}
+        />
+      ))}
     </S.Container>
   );
 };
