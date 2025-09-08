de4e2bc858d6899caee6cf4251f99c9975a41f35
7i.w__k <tizmfns3@gmail.com>
2025-09-08 14:15:02 +0900

    fix: s3 환경 변수 추가 - 3

--- backend/src/main/java/coffeeshout/global/config/AwsConfig.java
+++ backend/src/main/java/coffeeshout/global/config/AwsConfig.java
@@ -3,8 +3,7 @@ package coffeeshout.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -14,10 +13,17 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 public class AwsConfig {
 
     @Bean
+    public InstanceProfileCredentialsProvider instanceProfileCredentialsProvider() {
+        return InstanceProfileCredentialsProvider.builder()
+                .asyncCredentialUpdateEnabled(true)
+                .build();
+    }
+
+    @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(DefaultAwsRegionProviderChain.builder().build().getRegion())
-                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .credentialsProvider(instanceProfileCredentialsProvider())
                 .build();
     }
 
@@ -25,7 +31,7 @@ public class AwsConfig {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(DefaultAwsRegionProviderChain.builder().build().getRegion())
-                .credentialsProvider(DefaultCredentialsProvider.builder().build())
+                .credentialsProvider(instanceProfileCredentialsProvider())
                 .build();
     }
 }
