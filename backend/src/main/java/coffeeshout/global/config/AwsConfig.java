db3e2b971b7b963f7665aa3c4aa06d123a7742ea
7i.w__k <tizmfns3@gmail.com>
2025-09-08 13:51:00 +0900

    fix: s3 환경 변수 추가

--- backend/src/main/java/coffeeshout/global/config/AwsConfig.java
+++ backend/src/main/java/coffeeshout/global/config/AwsConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.core.SdkSystemSetting;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -26,7 +24,7 @@ public class AwsConfig {
     @Bean
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-                .region(Region.of(System.getenv(SdkSystemSetting.AWS_REGION.environmentVariable())))
+                .region(DefaultAwsRegionProviderChain.builder().build().getRegion())
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
