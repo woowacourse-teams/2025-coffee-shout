6572886a65fd3930274de7e541d3df9fad2ae843
SuHwa Lee <tnghk9611@naver.com>
2025-09-11 13:38:52 +0900

    [feat] QR코드로 방 입장 구현 (#642)
    * feat: JoinCodeModal UI 수정
    
    * feat: JoinCodeModal 스토리북 작성
    
    * feat: TabBar 컴포넌트 구현
    
    * refactor: Modal 제목 수정
    
    * refactor: story title 수정
    
    * feat: TabBar 스토리북 작성
    
    * feat: QRJoinPage 구현
    
    * feat: qrCodeUrl 전달
    
    * feat: 닉네임 검증 요청에 대한 api 에러 처리
    
    qrcode로 entry/name에 들어갔을 때, 유효하지 않은 방에 대한 에러 발생
    try-catch로 예외 처리
    
    * refactor: qr으로 들어가는 페이지 문구 수정
    
    * refactor: JoinCodeModal 이름 의미에 맞게 수정
    
    * refactor: 공유하는 공통 로직을 하나의 유틸 함수로 분리
    
    * refactor: 2개의 컴포넌트를 따로 분리
    
    * refactor: z-index 삭제
    
    * refactor: indicatorRef에 명시적인 타입으로 null 추가
    
    * refactor: activeTabIndex로 수정

---
+++ frontend/src/features/join/pages/QRJoinPage.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 16px;
+`;
