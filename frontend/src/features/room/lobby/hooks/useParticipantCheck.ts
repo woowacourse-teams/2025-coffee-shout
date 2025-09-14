f7e43468c5bd5177b20ec942f1b9209cdb7754f6
sooyeoniya <tndus1306403@naver.com>
2025-09-14 15:22:05 +0900

    chore: useParticipantCheck에 디버깅용 콘솔 추가

--- frontend/src/features/room/lobby/hooks/useParticipantCheck.ts
+++ frontend/src/features/room/lobby/hooks/useParticipantCheck.ts
@@ -15,13 +15,19 @@ export const useParticipantCheck = ({ isConnected }: Props) => {
 
   const checkUserExistsAndRedirect = useCallback(() => {
     if (!myName) {
+      console.log('해당 사용자 닉네임이 없음 - 홈으로 리디렉션');
       navigate('/', { replace: true });
       return;
     }
 
     const currentUser = participants.find((participant) => participant.playerName === myName);
 
+    console.log('participants: ', participants);
+    console.log('myName: ', myName);
+    console.log('currentUser: ', currentUser);
+
     if (!participants.length || !currentUser) {
+      console.log('사용자 정보에서 자기 자신을 찾을 수 없음 - 홈으로 리디렉션');
       navigate('/', { replace: true });
     }
   }, [myName, participants, navigate]);
