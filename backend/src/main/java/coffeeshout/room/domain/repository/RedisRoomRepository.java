7749ed216da1e93dabf476edb0202abeba93d09e
junhaa <wooha34567@gmail.com>
2025-09-15 16:23:51 +0900

    feat: RedisRepository 추가

---
+++ backend/src/main/java/coffeeshout/room/domain/repository/RedisRoomRepository.java
@@ -0,0 +1,40 @@
+package coffeeshout.room.domain.repository;
+
+import static org.springframework.util.Assert.notNull;
+
+import coffeeshout.room.domain.JoinCode;
+import coffeeshout.room.domain.Room;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRoomRepository implements RoomRepository {
+
+    private static final String ROOM_KEY = "room:%s";
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public Optional<Room> findByJoinCode(JoinCode joinCode) {
+        return Optional.of((Room) redisTemplate.opsForHash().get(String.format(ROOM_KEY, joinCode.getValue()), "info"));
+    }
+
+    @Override
+    public boolean existsByJoinCode(JoinCode joinCode) {
+        return true;
+    }
+
+    @Override
+    public Room save(Room room) {
+        redisTemplate.opsForHash().put(String.format(ROOM_KEY, room.getJoinCode().getValue()), "info", room);
+        return room;
+    }
+
+    @Override
+    public void deleteByJoinCode(JoinCode joinCode) {
+        notNull(joinCode, "JoinCode는 null일 수 없습니다.");
+        redisTemplate.opsForHash().delete(String.format(ROOM_KEY, joinCode.getValue()), "info");
+    }
+}
