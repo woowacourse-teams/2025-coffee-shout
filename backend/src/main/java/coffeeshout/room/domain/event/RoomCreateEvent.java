276edb8b215652ae831aa3defeb5a4b154315816
MinJun Choi <mj043000@naver.com>
2025-09-16 19:15:45 +0900

    refactor: RoomCreateEvent 클래스 Getter/Setter 추가 및 Jackson 어노테이션 제거
    - `RoomCreateEvent` 클래스에 Lombok 기반의 Getter/Setter 추가
    - Jackson 어노테이션 제거로 코드 간소화
    - 클래스 구조 간소화로 유지보수성 및 가독성 향상

--- backend/src/main/java/coffeeshout/room/domain/event/RoomCreateEvent.java
+++ backend/src/main/java/coffeeshout/room/domain/event/RoomCreateEvent.java
@@ -1,7 +1,6 @@
 package coffeeshout.room.domain.event;
 
 import coffeeshout.room.ui.request.SelectedMenuRequest;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Getter;
@@ -9,67 +8,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@Getter
+@Setter
 public class RoomCreateEvent {
 
-    @JsonProperty("eventId")
     private String eventId;
-    
-    @JsonProperty("hostName")
     private String hostName;
-    
-    @JsonProperty("selectedMenuRequest")
     private SelectedMenuRequest selectedMenuRequest;
-    
-    @JsonProperty("jode")
     private String joinCode;
-    
-    @JsonProperty("timestamp")
     private LocalDateTime timestamp;
-
-    @JsonProperty("eventId")
-    public String getEventId() {
-        return eventId;
-    }
-
-    @JsonProperty("hostName")
-    public String getHostName() {
-        return hostName;
-    }
-
-    @JsonProperty("selectedMenuRequest")
-    public SelectedMenuRequest getSelectedMenuRequest() {
-        return selectedMenuRequest;
-    }
-
-    @JsonProperty("jode")
-    public String getJoinCode() {
-        return joinCode;
-    }
-
-    @JsonProperty("timestamp")
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
-    }
-
-    public void setHostName(String hostName) {
-        this.hostName = hostName;
-    }
-
-    public void setSelectedMenuRequest(SelectedMenuRequest selectedMenuRequest) {
-        this.selectedMenuRequest = selectedMenuRequest;
-    }
-
-    public void setJoinCode(String joinCode) {
-        this.joinCode = joinCode;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 
     public static RoomCreateEvent create(String hostName, SelectedMenuRequest selectedMenuRequest, String joinCode) {
         final RoomCreateEvent event = new RoomCreateEvent();
