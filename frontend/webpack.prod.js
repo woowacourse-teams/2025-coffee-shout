e21bab03bd55f2ce3239f677aa5ac6b14a569cdf
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-09-16 10:19:34 +0900

    [chore] 웹팩 prod/dev 올바르게 설정되도록 변경  (#689)
    * feat : lazy import 적용
    
    * chore : 각 lazy 로딩 js에 이름 설정
    
    * chore : build될때 production으로 적용되도록 수정
    
    * chore : env 파일 분리 및 적용
    
    * fix : 웹팩 설정과 관련 없는거 되돌리기
    
    * fix : optimiztion 없애기
    
    * refactor : 필요없는 주석 제거

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,7 +1,7 @@
 import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
 
-export default merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-});
+export default (env, argv) =>
+  merge(common(env, { ...argv, mode: 'production' }), {
+    devtool: 'source-map',
+  });
