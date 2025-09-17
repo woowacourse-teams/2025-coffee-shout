207c7395996110bea24e0d401ef8c71319d47432
junhaa <wooha34567@gmail.com>
2025-09-17 16:38:53 +0900

    refactor: RoomService MiniGameServiceManager로 변경

--- backend/src/main/java/coffeeshout/minigame/application/MiniGameService.java
+++ backend/src/main/java/coffeeshout/minigame/application/MiniGameService.java
@@ -1,8 +1,17 @@
 package coffeeshout.minigame.application;
 
-import coffeeshout.room.domain.Playable;
+
+import coffeeshout.minigame.domain.MiniGameResult;
+import coffeeshout.minigame.domain.MiniGameScore;
+import coffeeshout.room.domain.player.Player;
+import java.util.List;
+import java.util.Map;
 
 public interface MiniGameService {
 
-    void start(Playable playable, String joinCode);
+    void start(String joinCode, List<Player> players);
+
+    Map<Player, MiniGameScore> getMiniGameScores(String joinCode);
+
+    MiniGameResult getMiniGameRanks(String joinCode);
 }
