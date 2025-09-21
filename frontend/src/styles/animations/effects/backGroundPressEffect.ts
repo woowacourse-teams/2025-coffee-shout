c99b7f16cd653eb7dc55163129073d853ced32d6
Ko Daeun <drechsu@gmail.com>
2025-09-22 00:10:28 +0900

    refactor : 배경색이 가운데서부터 퍼지는 효과 적용

--- frontend/src/styles/animations/effects/backGroundPressEffect.ts
+++ frontend/src/styles/animations/effects/backGroundPressEffect.ts
@@ -1,15 +1,43 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 
-interface BackgroundPressEffectProps {
+type Props = {
   activeColor: string;
   isTouching?: boolean;
-}
+};
 
-export const backgroundPressEffect = ({
-  activeColor,
-  isTouching,
-}: BackgroundPressEffectProps) => css`
-  transition: background-color 0.2s ease-in-out;
+const rippleKeyframes = keyframes`
+  from {
+    transform: scaleX(0);
+    opacity: 0.4;
+  }
+  to {
+    transform: scaleX(1);
+    opacity: 1;
+  }
+`;
 
-  ${isTouching && `background-color: ${activeColor};`}
+export const backgroundPressEffect = ({ activeColor, isTouching }: Props) => css`
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: ${activeColor};
+    transform: scaleX(0);
+    opacity: 0;
+    border-radius: 12px;
+  }
+
+  ${isTouching &&
+  css`
+    &::before {
+      animation: ${rippleKeyframes} 200ms ease-out forwards;
+    }
+  `}
 `;
