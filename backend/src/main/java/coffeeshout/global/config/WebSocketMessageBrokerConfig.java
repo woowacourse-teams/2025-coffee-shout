091b05102ccdccf3d8cc69ed74ee70f4800a13f5
MinJun Choi <mj043000@naver.com>
2025-09-24 12:30:52 +0900

    refactor: STOMP PreSend 및 PostSend 핸들러 제거 및 관련 테스트 삭제
    - ConnectPreSendHandler, ConnectPostSendHandler, CustomStompChannelInterceptor 제거로 불필요한 핸들러 정리
    - 관련된 테스트 코드 삭제
    - 핸들러 기반 로직을 Spring WebSocket 이벤트 기반으로 이전 완료
    - 코드 가독성 및 유지보수성 개선

--- backend/src/main/java/coffeeshout/global/config/WebSocketMessageBrokerConfig.java
+++ backend/src/main/java/coffeeshout/global/config/WebSocketMessageBrokerConfig.java
@@ -3,9 +3,7 @@ package coffeeshout.global.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -16,12 +14,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
     private final TaskScheduler taskScheduler;
-    private final ChannelInterceptor channelInterceptor;
 
-    public WebSocketMessageBrokerConfig(@Qualifier("webSocketHeartBeatScheduler") TaskScheduler taskScheduler,
-                                        ChannelInterceptor channelInterceptor) {
+    public WebSocketMessageBrokerConfig(@Qualifier("webSocketHeartBeatScheduler") TaskScheduler taskScheduler) {
         this.taskScheduler = taskScheduler;
-        this.channelInterceptor = channelInterceptor;
     }
 
     @Override
@@ -38,10 +33,5 @@ public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfi
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(channelInterceptor);
     }
 }
