bc89a816c4a1b0ac0423876bfef765e3cdbcb904
7i.w__k <tizmfns3@gmail.com>
2025-09-24 16:18:28 +0900

    fix: 웹소켓 연결 수 수정

--- backend/src/main/java/coffeeshout/global/config/WebSocketMessageBrokerConfig.java
+++ backend/src/main/java/coffeeshout/global/config/WebSocketMessageBrokerConfig.java
@@ -63,6 +63,6 @@ public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfi
                 .corePoolSize(9)
                 .maxPoolSize(18)
                 .queueCapacity(Integer.MAX_VALUE)
-                .keepAliveSeconds(30);
+                .keepAliveSeconds(60);
     }
 }
--- backend/src/main/java/coffeeshout/global/config/WebSocketSchedulerConfig.java
+++ backend/src/main/java/coffeeshout/global/config/WebSocketSchedulerConfig.java
@@ -9,21 +9,21 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @Configuration
 public class WebSocketSchedulerConfig {
 
-    @Bean(name = "webSocketHeartBeatScheduler")
-    public TaskScheduler heartBeatMessageBrokerTaskScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(1);
-        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
-        scheduler.initialize();
-        return scheduler;
-    }
-
     @Bean
     @Primary
     public TaskScheduler applicationTaskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(4);
         scheduler.setThreadNamePrefix("app-scheduler-");
+        scheduler.initialize();
+        return scheduler;
+    }
+
+    @Bean(name = "webSocketHeartBeatScheduler")
+    public TaskScheduler heartBeatMessageBrokerTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
         scheduler.initialize();
         return scheduler;
     }
