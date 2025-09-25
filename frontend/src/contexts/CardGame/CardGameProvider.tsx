2d1d58fc79fb6a97e539abcbcacf7185b6a0100a
SuHwa Lee <tnghk9611@naver.com>
2025-09-25 14:05:58 +0900

    [refactor] CardGameProvider 로직 분리 (#740)
    * refactor: handleCardGameState 함수 핸들러 분리
    
    * refactor: 선택된 카드를 업데이트하는 부분 공통 함수로 분리
    
    * refactor: 커스텀 훅 분리
    
    * refactor: 상태랑 update 함수 순서 변경
    
    * refactor: 불필요한 setState 내보내지 않도록 수정
    
    * refactor: 상태 기반의 action을 관리하는 reducer 구조로 변경

--- frontend/src/contexts/CardGame/CardGameProvider.tsx
+++ frontend/src/contexts/CardGame/CardGameProvider.tsx
@@ -1,135 +1,22 @@
 import { useWebSocketSubscription } from '@/apis/websocket/hooks/useWebSocketSubscription';
-import { CardGameState, CardInfo, SelectedCardInfo } from '@/types/miniGame/cardGame';
-import { RoundType } from '@/types/miniGame/round';
-import { PropsWithChildren, useCallback, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { PropsWithChildren } from 'react';
 import { useIdentifier } from '../Identifier/IdentifierContext';
 import { CardGameContext } from './CardGameContext';
-
-type CardGameStateResponse = {
-  cardGameState: CardGameState;
-  currentRound: RoundType;
-  cardInfoMessages: CardInfo[];
-  allSelected: boolean;
-};
+import { useCardGameState } from './hooks/useCardGameState';
+import { useSelectedCard } from './hooks/useSelectedCard';
+import { useCardGameHandlers } from './hooks/useCardGameHandlers';
 
 const CardGameProvider = ({ children }: PropsWithChildren) => {
-  const navigate = useNavigate();
   const { joinCode, myName } = useIdentifier();
-  const { miniGameType } = useParams();
-  const [isTransition, setIsTransition] = useState<boolean>(false);
-  const [currentRound, setCurrentRound] = useState<RoundType>('FIRST');
-  const [currentCardGameState, setCurrentCardGameState] = useState<CardGameState>('READY');
-  const [cardInfos, setCardInfos] = useState<CardInfo[]>([]);
-  const [selectedCardInfo, setSelectedCardInfo] = useState<SelectedCardInfo>({
-    FIRST: {
-      isSelected: false,
-      type: null,
-      value: null,
-    },
-    SECOND: {
-      isSelected: false,
-      type: null,
-      value: null,
-    },
+
+  const { dispatch, isTransition, currentRound, currentCardGameState, cardInfos } =
+    useCardGameState();
+
+  const { selectedCardInfo, updateSelectedCardInfo } = useSelectedCard(myName);
+
+  const { handleCardGameState } = useCardGameHandlers(dispatch, {
+    updateSelectedCardInfo,
   });
-
-  const handleCardGameState = useCallback(
-    (data: CardGameStateResponse) => {
-      const { cardGameState, currentRound, cardInfoMessages } = data;
-
-      const isPreparing = cardGameState === 'PREPARE';
-      const isFirstRoundPlaying = cardGameState === 'PLAYING' && currentRound === 'FIRST';
-      const isFirstRoundScoreBoard = cardGameState === 'SCORE_BOARD' && currentRound === 'FIRST';
-      const isSecondRoundLoading = cardGameState === 'LOADING' && currentRound === 'SECOND';
-      const isSecondRoundPlaying = cardGameState === 'PLAYING' && currentRound === 'SECOND';
-      const isSecondRoundScoreBoard = cardGameState === 'SCORE_BOARD' && currentRound === 'SECOND';
-      const isGameDone = cardGameState === 'DONE';
-
-      const handleScoreBoard = () => {
-        setCurrentCardGameState('SCORE_BOARD');
-        setCardInfos(cardInfoMessages);
-
-        const mySelectedCardInfo = cardInfoMessages.find((card) => card.playerName === myName);
-        if (!mySelectedCardInfo) return;
-        if (selectedCardInfo[currentRound].isSelected) return;
-
-        setSelectedCardInfo((prev) => ({
-          ...prev,
-          [currentRound]: {
-            isSelected: true,
-            type: mySelectedCardInfo.cardType,
-            value: mySelectedCardInfo.value,
-          },
-        }));
-      };
-
-      if (isPreparing) {
-        setCurrentCardGameState('PREPARE');
-        setCardInfos(cardInfoMessages);
-        return;
-      }
-
-      if (isFirstRoundPlaying) {
-        setCurrentCardGameState('PLAYING');
-        setCardInfos(cardInfoMessages);
-
-        const myCardInfo = cardInfoMessages.find((card) => card.playerName === myName);
-        if (!myCardInfo) return;
-
-        setSelectedCardInfo((prev) => ({
-          ...prev,
-          [currentRound]: {
-            isSelected: true,
-            type: myCardInfo.cardType,
-            value: myCardInfo.value,
-          },
-        }));
-        return;
-      }
-
-      if (isFirstRoundScoreBoard) {
-        handleScoreBoard();
-        return;
-      }
-
-      if (isSecondRoundLoading) {
-        setIsTransition(true);
-        setCurrentRound('SECOND');
-        setCurrentCardGameState('LOADING');
-        return;
-      }
-
-      if (isSecondRoundPlaying) {
-        setIsTransition(false);
-        setCardInfos(cardInfoMessages);
-        setCurrentCardGameState('PLAYING');
-        const myCardInfo = cardInfoMessages.find((card) => card.playerName === myName);
-        if (!myCardInfo) return;
-
-        setSelectedCardInfo((prev) => ({
-          ...prev,
-          [currentRound]: {
-            isSelected: true,
-            type: myCardInfo.cardType,
-            value: myCardInfo.value,
-          },
-        }));
-        return;
-      }
-
-      if (isSecondRoundScoreBoard) {
-        handleScoreBoard();
-        return;
-      }
-
-      if (isGameDone) {
-        navigate(`/room/${joinCode}/${miniGameType}/result`);
-        return;
-      }
-    },
-    [navigate, joinCode, miniGameType, myName, setSelectedCardInfo, selectedCardInfo]
-  );
 
   useWebSocketSubscription(`/room/${joinCode}/gameState`, handleCardGameState);
 
@@ -141,7 +28,6 @@ const CardGameProvider = ({ children }: PropsWithChildren) => {
         currentCardGameState,
         cardInfos,
         selectedCardInfo,
-        setSelectedCardInfo,
       }}
     >
       {children}
---
+++ frontend/src/contexts/CardGame/hooks/useSelectedCard.ts
@@ -0,0 +1,42 @@
+import { CardInfo, SelectedCardInfo } from '@/types/miniGame/cardGame';
+import { RoundType } from '@/types/miniGame/round';
+import { useCallback, useState } from 'react';
+
+export const useSelectedCard = (myName: string) => {
+  const [selectedCardInfo, setSelectedCardInfo] = useState<SelectedCardInfo>({
+    FIRST: {
+      isSelected: false,
+      type: null,
+      value: null,
+    },
+    SECOND: {
+      isSelected: false,
+      type: null,
+      value: null,
+    },
+  });
+
+  const updateSelectedCardInfo = useCallback(
+    (cardInfoMessages: CardInfo[], round: RoundType, shouldCheckAlreadySelected = false) => {
+      const myCardInfo = cardInfoMessages.find((card) => card.playerName === myName);
+      if (!myCardInfo) return;
+
+      if (shouldCheckAlreadySelected && selectedCardInfo[round].isSelected) return;
+
+      setSelectedCardInfo((prev) => ({
+        ...prev,
+        [round]: {
+          isSelected: true,
+          type: myCardInfo.cardType,
+          value: myCardInfo.value,
+        },
+      }));
+    },
+    [myName, selectedCardInfo]
+  );
+
+  return {
+    selectedCardInfo,
+    updateSelectedCardInfo,
+  };
+};
