a0dbc9b4a9b3ca4f40a3ea2174ce457f6d5e751c
Ko Daeun <drechsu@gmail.com>
2025-09-29 21:47:28 +0900

    refactor : scaleValue도 옵셔널로 받을 수 있게 수정

--- frontend/src/styles/animations/buttonHoverPress.ts
+++ frontend/src/styles/animations/buttonHoverPress.ts
@@ -2,18 +2,21 @@ import { css } from '@emotion/react';
 import { TouchState } from '@/types/touchState';
 import { backgroundPressEffect } from './effects/backGroundPressEffect';
 import { scalePressEffect } from './effects/scalePressEffect';
+import { ANIMATION_SCALE } from '@/constants/animation';
 
-interface ButtonHoverPressProps {
+type Props = {
   activeColor: string;
   touchState: TouchState;
   enableScale?: boolean;
-}
+  scaleValue?: number;
+};
 
 export const buttonHoverPress = ({
   activeColor,
   touchState,
   enableScale = true,
-}: ButtonHoverPressProps) => {
+  scaleValue = ANIMATION_SCALE.SCALE_PRESS,
+}: Props) => {
   return css`
     /* 데스크톱: hover 효과 */
     @media (hover: hover) and (pointer: fine) {
@@ -24,6 +27,6 @@ export const buttonHoverPress = ({
 
     /* 터치 디바이스: touchState 상태로 제어 */
     ${backgroundPressEffect({ activeColor, touchState })}
-    ${enableScale && scalePressEffect({ touchState, scaleValue: 0.98 })}
+    ${enableScale && scalePressEffect({ touchState, scaleValue })}
   `;
 };
