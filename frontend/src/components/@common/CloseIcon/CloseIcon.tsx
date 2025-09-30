856280cf94dbbcb42d601fc6f50e1dbbb489374b
SuHwa Lee <tnghk9611@naver.com>
2025-09-30 21:27:33 +0900

    chore: 크로마틱 설치

--- frontend/src/components/@common/CloseIcon/CloseIcon.tsx
+++ frontend/src/components/@common/CloseIcon/CloseIcon.tsx
@@ -17,6 +17,7 @@ const CloseIcon = ({ stroke, strokeWidth = 2, ...rest }: Props) => {
       viewBox="0 0 19 19"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-label="close-icon"
       {...rest}
     >
       <path
