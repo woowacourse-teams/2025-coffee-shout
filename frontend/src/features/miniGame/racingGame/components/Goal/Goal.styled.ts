c90eb03539fed9a8e9372a9fd4f15261747e9513
Ko Daeun <drechsu@gmail.com>
2025-10-01 15:48:01 +0900

    feat : goal컴포넌트 구현

---
+++ frontend/src/features/miniGame/racingGame/components/Goal/Goal.styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
