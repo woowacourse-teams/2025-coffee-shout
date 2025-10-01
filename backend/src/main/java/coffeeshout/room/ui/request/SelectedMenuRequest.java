b5728d6ed207138bd34a9eb01e7439686af50198
7i.w__k <tizmfns3@gmail.com>
2025-10-01 12:34:05 +0900

    fix: customName NotBlank에서 NotNull 처리

--- backend/src/main/java/coffeeshout/room/ui/request/SelectedMenuRequest.java
+++ backend/src/main/java/coffeeshout/room/ui/request/SelectedMenuRequest.java
@@ -1,10 +1,9 @@
 package coffeeshout.room.ui.request;
 
 import coffeeshout.room.domain.menu.MenuTemperature;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record SelectedMenuRequest(@NotNull Long id, @NotBlank String customName,
+public record SelectedMenuRequest(@NotNull Long id, @NotNull String customName,
                                   @NotNull MenuTemperature temperature) {
 
 }
