c9abada9da10d6306c7e835c9c2bd27717c953e7
HyeonsuLee <leehyeonsu4888@naver.com>
2025-10-01 14:27:00 +0900

    fix: room enter not null 제거

--- backend/src/main/java/coffeeshout/room/ui/request/SelectedMenuRequest.java
+++ backend/src/main/java/coffeeshout/room/ui/request/SelectedMenuRequest.java
@@ -1,9 +1,7 @@
 package coffeeshout.room.ui.request;
 
 import coffeeshout.room.domain.menu.MenuTemperature;
-import jakarta.validation.constraints.NotNull;
 
-public record SelectedMenuRequest(@NotNull Long id, @NotNull String customName,
-                                  @NotNull MenuTemperature temperature) {
+public record SelectedMenuRequest(Long id, String customName, MenuTemperature temperature) {
 
 }
