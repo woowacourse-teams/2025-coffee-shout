3fa8f5a0893f4bc9166e45df701f1d984e4ec733
MinJun Choi <mj043000@naver.com>
2025-10-01 15:34:10 +0900

    refactor: MiniGameResultJpaRepository 메서드 정리 및 불필요한 코드 제거
    - MiniGameResultJpaRepository에서 사용되지 않는 findByMiniGamePlay, findByPlayer 메서드 제거
    - 불필요한 코드 삭제로 인터페이스 간소화 및 유지보수성 향상

--- backend/src/main/java/coffeeshout/minigame/infra/persistance/MiniGameResultJpaRepository.java
+++ backend/src/main/java/coffeeshout/minigame/infra/persistance/MiniGameResultJpaRepository.java
@@ -1,16 +1,10 @@
 package coffeeshout.minigame.infra.persistance;
 
-import coffeeshout.room.infra.persistance.PlayerEntity;
-import java.util.List;
 import org.springframework.data.repository.Repository;
 
 public interface MiniGameResultJpaRepository extends Repository<MiniGameResultEntity, Long> {
 
     MiniGameResultEntity save(MiniGameResultEntity miniGameResultEntity);
-
-    List<MiniGameResultEntity> findByMiniGamePlay(MiniGameEntity miniGamePlay);
-
-    List<MiniGameResultEntity> findByPlayer(PlayerEntity player);
 
     void deleteAll();
 }
