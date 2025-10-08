5154108c8af678491e617087f1c1ba6b9f5bf955
Ko Daeun <drechsu@gmail.com>
2025-10-08 17:04:17 +0900

    feat : 탭 담당 send 로직 작성

--- frontend/src/features/miniGame/racingGame/components/RacingGameOverlay/RacingGameOverlay.tsx
+++ frontend/src/features/miniGame/racingGame/components/RacingGameOverlay/RacingGameOverlay.tsx
@@ -1,14 +1,22 @@
-import { ReactNode, MouseEvent } from 'react';
+import { ReactNode, MouseEvent, TouchEvent } from 'react';
 import * as S from './RacingGameOverlay.styled';
+import { useWebSocket } from '@/apis/websocket/contexts/WebSocketContext';
+import { useIdentifier } from '@/contexts/Identifier/IdentifierContext';
 
 type Props = {
   children: ReactNode;
 };
 
 const RacingGameOverlay = ({ children }: Props) => {
-  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+  const { joinCode, myName } = useIdentifier();
+  const { send } = useWebSocket();
+  const handleClick = (event: MouseEvent<HTMLDivElement> | TouchEvent<HTMLDivElement>) => {
     event.stopPropagation();
-    console.log('clicked');
+    console.log('clicked tabCount: 30');
+    send(`/room/${joinCode}/racing-game/tap`, {
+      playerName: myName,
+      tabCount: 30,
+    });
   };
 
   return <S.Overlay onClick={handleClick}>{children}</S.Overlay>;
