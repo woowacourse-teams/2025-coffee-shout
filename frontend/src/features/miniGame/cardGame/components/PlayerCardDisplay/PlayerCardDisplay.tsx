e6d933fda6ed2c80062318ca385246438f5c0d5f
Daeun-100 <141714293+Daeun-100@users.noreply.github.com>
2025-10-10 16:08:11 +0900

    [feat] 카드게임에서 카드 눌렀을 때 효과 추가 (#836)
    * feat : 카드 클릭시 뒤집어지는 효과 적용
    
    * refactor : flip 속도 조정 가능하게 변경
    
    * refactor : 플레이어 카드에도 flip 적용
    
    * refactor : 카드 뒤집어지는 속도 빠르게 조정
    
    * refactor : navigate 주석 제거

--- frontend/src/features/miniGame/cardGame/components/PlayerCardDisplay/PlayerCardDisplay.tsx
+++ frontend/src/features/miniGame/cardGame/components/PlayerCardDisplay/PlayerCardDisplay.tsx
@@ -2,6 +2,8 @@ import { Card, CardGameRound, SelectedCardInfo } from '@/types/miniGame/cardGame
 import CardBack from '../CardBack/CardBack';
 import CardFront from '../CardFront/CardFront';
 import * as S from './PlayerCardDisplay.styled';
+import Flip from '@/components/@common/Flip/Flip';
+import { DESIGN_TOKENS } from '@/constants/design';
 
 type Props = {
   selectedCardInfo: SelectedCardInfo;
@@ -11,21 +13,26 @@ const PlayerCardDisplay = ({ selectedCardInfo }: Props) => {
   const renderPlayerCard = (round: CardGameRound) => {
     const cardInfo = selectedCardInfo[round];
 
-    if (cardInfo.isSelected) {
-      return (
-        <CardFront
-          size="medium"
-          card={
-            {
-              type: cardInfo.type,
-              value: cardInfo.value,
-            } as Card
-          }
-        />
-      );
-    }
-
-    return <CardBack size="medium" disabled={true} />;
+    return (
+      <Flip
+        flipped={cardInfo.isSelected}
+        width={DESIGN_TOKENS.card.medium.width}
+        height={DESIGN_TOKENS.card.medium.height}
+        duration={0.5}
+        initialView={<CardBack size="medium" disabled={true} />}
+        flippedView={
+          <CardFront
+            size="medium"
+            card={
+              {
+                type: cardInfo.type,
+                value: cardInfo.value,
+              } as Card
+            }
+          />
+        }
+      />
+    );
   };
 
   return (
