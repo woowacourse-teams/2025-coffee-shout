4ce2ffa29ed0cf8d664b5591f391a37931451b99
leehyeonsu <leehyeonsu4888@gmail.com>
2025-10-12 20:48:18 +0900

    refactor: 예외 메시지 수정

--- backend/src/main/java/coffeeshout/racinggame/domain/RacingRange.java
+++ backend/src/main/java/coffeeshout/racinggame/domain/RacingRange.java
@@ -7,7 +7,7 @@ public record RacingRange(int start, int end) {
             throw new IllegalArgumentException("RacingRange의 start와 end는 0보다 커야합니다.");
         }
         if (start > end) {
-            throw new IllegalArgumentException("RacingRange의 start는 end보다 커야 합니다.");
+            throw new IllegalArgumentException("RacingRange의 start는 end보다 작아야 합니다.");
         }
     }
 
