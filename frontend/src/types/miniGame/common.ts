4d4d9b789d53c0522c66b000d1e44ef3444f0165
Ko Daeun <drechsu@gmail.com>
2025-10-14 14:27:14 +0900

    feat : 레이싱 게임 아이콘 추가 및 매핑적용

--- frontend/src/types/miniGame/common.ts
+++ frontend/src/types/miniGame/common.ts
@@ -1,3 +1,6 @@
+import CardIcon from '@/assets/card-icon.svg';
+import RacingIcon from '@/assets/racing-icon.svg';
+
 /**
  * 전체 미니 게임 공통 타입
  */
@@ -13,4 +16,9 @@ export type MiniGameType = keyof typeof MINI_GAME_NAME_MAP;
 export const MINI_GAME_DESCRIPTION_MAP: Record<MiniGameType, string[]> = {
   CARD_GAME: ['2라운드 동안 매번 카드 1장씩 뒤집어', '가장 높은 점수를 내보세요!'],
   RACING_GAME: ['화면을 클릭해 속도를 높여서', '가장 먼저 도착하세요!'],
+};
+
+export const MINI_GAME_ICON_MAP: Record<MiniGameType, string> = {
+  CARD_GAME: CardIcon,
+  RACING_GAME: RacingIcon,
 };
