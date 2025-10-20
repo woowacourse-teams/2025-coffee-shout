13aeca4848557f5ff713feceb2d2d4f1be1ec162
Ko Daeun <drechsu@gmail.com>
2025-10-20 15:26:38 +0900

    refactor : FadeInItem 컴포넌트에서 PropsWithChildren 쓰도록 변경

--- frontend/src/components/@common/FadeInItem/FadeInItem.tsx
+++ frontend/src/components/@common/FadeInItem/FadeInItem.tsx
@@ -1,14 +1,13 @@
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import * as S from './FadeInItem.styled';
 
 type Props = {
-  children: ReactNode;
   index?: number;
   delay?: number;
   duration?: number;
-};
+} & PropsWithChildren;
 
-const FadeInItem = ({ children, index = 0, delay = 200, duration = 600 }: Props) => {
+const FadeInItem = ({ index = 0, delay = 200, duration = 600, children }: Props) => {
   return (
     <S.Wrapper $index={index} $delay={delay} $duration={duration}>
       {children}
