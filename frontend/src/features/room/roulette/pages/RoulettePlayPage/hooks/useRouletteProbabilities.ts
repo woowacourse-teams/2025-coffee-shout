9dfcc758693babfb0b23a3d11da89cae195a9ed2
Ko Daeun <drechsu@gmail.com>
2025-10-21 14:15:29 +0900

    feat: 룰렛 첫 로딩 식별 ref를 페이지단에서 불러옴

--- frontend/src/features/room/roulette/pages/RoulettePlayPage/hooks/useRouletteProbabilities.ts
+++ frontend/src/features/room/roulette/pages/RoulettePlayPage/hooks/useRouletteProbabilities.ts
@@ -3,25 +3,24 @@ import { useIdentifier } from '@/contexts/Identifier/IdentifierContext';
 import { useParticipants } from '@/contexts/Participants/ParticipantsContext';
 import { useProbabilityHistory } from '@/contexts/ProbabilityHistory/ProbabilityHistoryContext';
 import { colorList } from '@/constants/color';
-import { useRef } from 'react';
+import { RefObject } from 'react';
 
 type ProbabilityResponse = {
   playerName: string;
   probability: number;
 };
 
-const useRouletteProbabilities = () => {
+const useRouletteProbabilities = (isFirstLoadRef: RefObject<boolean>) => {
   const { joinCode } = useIdentifier();
   const { getParticipantColorIndex } = useParticipants();
   const { updateCurrentProbabilities } = useProbabilityHistory();
-  const isFirst = useRef(false);
 
   const { loading: isLoading } = useFetch<ProbabilityResponse[]>({
     endpoint: `/rooms/${joinCode}/probabilities`,
     enabled: !!joinCode,
     onSuccess: (data) => {
-      if (isFirst.current) return;
-      isFirst.current = true;
+      if (!isFirstLoadRef.current) return;
+      isFirstLoadRef.current = false;
       updateCurrentProbabilities(
         data.map((probability) => ({
           ...probability,
