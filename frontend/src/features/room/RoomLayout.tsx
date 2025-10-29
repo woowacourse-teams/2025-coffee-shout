621f867e1ed8dce69d3965bebdf968195825a667
sooyeoniya <tndus1306403@naver.com>
2025-10-29 17:51:05 +0900

    feat: 라우팅 경로에 따라 메시지 분기 처리

--- frontend/src/features/room/RoomLayout.tsx
+++ frontend/src/features/room/RoomLayout.tsx
@@ -2,16 +2,22 @@ import { useBackButtonConfirm } from '@/hooks/useBackButtonConfirm';
 import { useNavigationGuard } from '@/hooks/useNavigateGuard';
 import { useReplaceNavigate } from '@/hooks/useReplaceNavigate';
 import { useRoomAccessGuard } from '@/hooks/useRoomAccessGuard';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const RoomLayout = () => {
   useNavigationGuard();
   useRoomAccessGuard();
 
+  const location = useLocation();
   const navigate = useReplaceNavigate();
+
+  const isRouletteOrOrderPath = /(roulette|order)/.test(location.pathname);
+
   useBackButtonConfirm({
     onConfirm: () => navigate('/'),
-    message: '정말 방에서 나가시겠습니까?\n방에서 나가도 게임은 계속 진행됩니다.',
+    message: isRouletteOrOrderPath
+      ? '정말 방에서 나가시겠습니까?'
+      : '게임 방에서 나가시겠습니까?\n방에서 나가도 게임은 계속 진행됩니다.',
   });
 
   return <Outlet />;
