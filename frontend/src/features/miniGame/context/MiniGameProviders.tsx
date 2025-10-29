c19a03f0810f41b9a8071de2340cf09276dc22e3
sooyeoniya <tndus1306403@naver.com>
2025-10-29 12:17:11 +0900

    fix: useBackButtonConfirm 훅 적용 위치 변경
    MiniGameProviders => RoomLayout 으로 위치 변경
    
    그 이유: 로비에서 게임 진입하는 시점부터 스택을 하나 더 쌓았기 때문에, 룰렛 화면부터 주문 화면까지도 뒤로가기 경고창을 막아주지 않으면, 뒤로가기 버튼을 눌렀을 때 그냥 로비로 나가져버림.. 그래서 직접 경고창을 통해 막아주고, 뒤로가기 확인했을 때 메인으로 돌아가게 해야함

--- frontend/src/features/miniGame/context/MiniGameProviders.tsx
+++ frontend/src/features/miniGame/context/MiniGameProviders.tsx
@@ -1,17 +1,9 @@
-import { useBackButtonConfirm } from '@/hooks/useBackButtonConfirm';
-import { useReplaceNavigate } from '@/hooks/useReplaceNavigate';
 import { MiniGameType } from '@/types/miniGame/common';
 import { PropsWithChildren } from 'react';
 import { useParams } from 'react-router-dom';
 import { GAME_CONFIGS } from '../config/gameConfigs';
 
 const MiniGameProviders = ({ children }: PropsWithChildren) => {
-  const navigate = useReplaceNavigate();
-  useBackButtonConfirm({
-    onConfirm: () => navigate('/'),
-    message: '게임 방에서 나가시겠습니까?\n방에서 나가져도 게임이 계속 진행됩니다.',
-  });
-
   const { miniGameType } = useParams();
   if (!miniGameType || !(miniGameType in GAME_CONFIGS)) {
     return <>{children}</>;
--- frontend/src/features/room/RoomLayout.tsx
+++ frontend/src/features/room/RoomLayout.tsx
@@ -1,10 +1,19 @@
+import { useBackButtonConfirm } from '@/hooks/useBackButtonConfirm';
 import { useNavigationGuard } from '@/hooks/useNavigateGuard';
+import { useReplaceNavigate } from '@/hooks/useReplaceNavigate';
 import { useRoomAccessGuard } from '@/hooks/useRoomAccessGuard';
 import { Outlet } from 'react-router-dom';
 
 const RoomLayout = () => {
   useNavigationGuard();
   useRoomAccessGuard();
+
+  const navigate = useReplaceNavigate();
+  useBackButtonConfirm({
+    onConfirm: () => navigate('/'),
+    message: '정말 방에서 나가시겠습니까?\n방에서 나가도 게임은 계속 진행됩니다.',
+  });
+
   return <Outlet />;
 };
 
